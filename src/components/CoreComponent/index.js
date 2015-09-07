6d4c9e881fd53ac1ef34d3bead6db915414451f8
Nicolas Gallagher <nicolasgallagher@twitter.com>
2015-09-07 12:58:48 -0700

    Move CoreComponent module

--- src/components/CoreComponent/index.js
+++ src/components/CoreComponent/index.js
@@ -1,3 +1,42 @@
-import CoreComponent from './modules/CoreComponent'
+import React, { PropTypes } from 'react'
+import restyle from './modules/restyle'
+import stylePropTypes from './modules/stylePropTypes'
+
+class CoreComponent extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+    component: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.string
+    ]),
+    style: PropTypes.object,
+    testID: PropTypes.string
+  }
+
+  static stylePropTypes = stylePropTypes;
+
+  static defaultProps = {
+    className: '',
+    component: 'div'
+  }
+
+  render() {
+    const {
+      className,
+      component: Component,
+      style,
+      testID,
+      ...other
+    } = this.props
+
+    return (
+      <Component
+        {...other}
+        {...restyle({ className, style })}
+        data-testid={process.env.NODE_ENV === 'production' ? null : testID}
+      />
+    )
+  }
+}
 
 export default CoreComponent
