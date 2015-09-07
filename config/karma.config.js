2e087c10ad5f58dbe218c8df23b11b1f46b100f1
Nicolas Gallagher <nicolasgallagher@twitter.com>
2015-09-07 09:51:09 -0700

    Add karma-firefox-launcher

--- config/karma.config.js
+++ config/karma.config.js
@@ -22,6 +22,7 @@ module.exports = function (config) {
     ],
     plugins: [
       'karma-chrome-launcher',
+      'karma-firefox-launcher',
       'karma-mocha',
       'karma-sourcemap-loader',
       'karma-webpack'
