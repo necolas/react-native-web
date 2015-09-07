cd215a916d80a7db1d23c14ad46126fde576110f
Nicolas Gallagher <nicolasgallagher@twitter.com>
2015-09-07 09:44:18 -0700

    Remove legacy react-native-web-style module

--- src/modules/react-native-web-style/modules/StylePropTypes/Background.js
+++
@@ -1,9 +0,0 @@
-import { PropTypes } from 'react'
-
-export default {
-  backgroundColor: PropTypes.string,
-  backgroundImage: PropTypes.string,
-  backgroundPosition: PropTypes.string,
-  backgroundRepeat: PropTypes.string,
-  backgroundSize: PropTypes.string
-}
--- src/modules/react-native-web-style/modules/StylePropTypes/Layout.js
+++
@@ -1,9 +0,0 @@
-import BoxModel from './BoxModel'
-import Flexbox from './Flexbox'
-import Position from './Position'
-
-export default {
-  ...BoxModel,
-  ...Flexbox,
-  ...Position
-}
