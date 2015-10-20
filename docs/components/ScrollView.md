809913175d73101b479a0db33e0c3e867e717671
Tom Ashworth <tashworth@twitter.com>
2015-10-20 15:57:51 -0700

    [add] ScrollView
    - Supports contentContainerStyle, horizontal, onScroll, scrollEnabled,
    scrollEventThrottle and style
    - Not tested well enough
    - Some docs
    
    Fix #6

--- docs/components/ScrollView.md
+++ docs/components/ScrollView.md
@@ -1,6 +1,6 @@
 # ScrollView
 
-TODO
+Scrollable view for use with bounded height, either by setting the height of the view directly (discouraged) or bounding the height of all parent views.
 
 ## Props
 
@@ -11,7 +11,7 @@ Child content.
 **contentContainerStyle**: style
 
 These styles will be applied to the scroll view content container which wraps
-all of the child views. Example:
+all of the child views.
 
 **horizontal**: bool = false
 
@@ -40,20 +40,45 @@ scrolled.)
 ## Examples
 
 ```js
-import React, { ScrollView } from 'react-native-web'
+import React, { StyleSheet, ScrollView } from 'react-native-web'
 
-const { Component, PropTypes } = React;
+import Item from './Item'
 
-class Example extends Component {
-  static propTypes = {
+export default class App extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      items: Array.from({ length: 20 }).map((_, i) => ({
+        id: i
+      }))
+    }
   }
 
-  static defaultProps = {
+  onScroll(e) {
+    console.log(e)
   }
 
   render() {
     return (
-    )
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContainer}
+        onScroll={e => this.onScroll(e)}
+        horizontal
+        scrollEventThrottle={1}
+        style={styles.scrollView}
+      >
+        {this.state.items.map(item => <Item {...item}/>)}
+      </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    borderWidth: '1px'
+  },
+  scrollViewContainer: {
+    padding: '10px'
+  }
+})
 ```
--- src/components/ScrollView/index.js
+++ src/components/ScrollView/index.js
@@ -1,18 +1,141 @@
+import debounce from 'lodash.debounce'
 import React, { PropTypes } from 'react'
+import { pickProps } from '../../modules/filterObjectProps'
+import StyleSheet from '../../modules/StyleSheet'
 import View from '../View'
+import ScrollViewStylePropTypes from './ScrollViewStylePropTypes'
+
+const scrollViewStyleKeys = Object.keys(ScrollViewStylePropTypes)
+
+const styles = StyleSheet.create({
+  initial: {
+    flexShrink: 1,
+    flexGrow: 1,
+    overflow: 'scroll'
+  },
+  contentContainerStyleInitial: {
+    flexShrink: 1,
+    flexGrow: 1
+  },
+  contentContainerStyleHorizontal: {
+    flexDirection: 'row'
+  }
+})
 
 class ScrollView extends React.Component {
   static propTypes = {
-    children: PropTypes.any
+    children: PropTypes.any,
+    contentContainerStyle: PropTypes.shape(ScrollViewStylePropTypes),
+    horizontal: PropTypes.bool,
+    onScroll: PropTypes.func,
+    scrollEnabled: PropTypes.bool,
+    scrollEventThrottle: PropTypes.number,
+    style: PropTypes.shape(ScrollViewStylePropTypes)
   }
 
   static defaultProps = {
-    className: ''
+    className: '',
+    style: styles.initial,
+    horizontal: false,
+    scrollEnabled: true,
+    scrollEventThrottle: 0
+  }
+
+  constructor(...args) {
+    super(...args)
+    this._debouncedOnScrollEnd = debounce(this._onScrollEnd, 100)
+    this.state = {
+      isScrolling: false
+    }
+  }
+
+  _onScroll(e) {
+    const { scrollEventThrottle } = this.props
+    const { isScrolling, scrollLastTick } = this.state
+
+    // A scroll happened, so the scroll bump the debouce.
+    this._debouncedOnScrollEnd(e)
+
+    if (isScrolling) {
+      // Scroll last tick may have changed, check if we need to notify
+      if (this._shouldEmitScrollEvent(scrollLastTick, scrollEventThrottle)) {
+        this._onScrollTick(e)
+      }
+    } else {
+      // Weren't scrolling, so we must have just started
+      this._onScrollStart(e)
+    }
+  }
+
+  _onScrollStart() {
+    this.setState({
+      isScrolling: true,
+      scrollLastTick: Date.now()
+    })
+  }
+
+  _onScrollTick(e) {
+    const { onScroll } = this.props
+    this.setState({
+      scrollLastTick: Date.now()
+    })
+    if (onScroll) onScroll(e)
+  }
+
+  _onScrollEnd(e) {
+    const { onScroll } = this.props
+    this.setState({
+      isScrolling: false
+    })
+    if (onScroll) onScroll(e)
+  }
+
+  _shouldEmitScrollEvent(lastTick, eventThrottle) {
+    const timeSinceLastTick = Date.now() - lastTick
+    return (eventThrottle > 0 && timeSinceLastTick > (1000 / eventThrottle))
+  }
+
+  _maybePreventScroll(e) {
+    const { scrollEnabled } = this.props
+    if (!scrollEnabled) {
+      e.preventDefault()
+    }
   }
 
   render() {
+    const {
+      children,
+      contentContainerStyle,
+      horizontal,
+      style
+    } = this.props
+
+    const resolvedStyle = pickProps(style, scrollViewStyleKeys)
+    const resolvedContentContainerStyle = pickProps(contentContainerStyle, scrollViewStyleKeys)
+
     return (
-      <View {...this.props} />
+      <View
+        _className='ScrollView'
+        onScroll={e => this._onScroll(e)}
+        onTouchMove={e => this._maybePreventScroll(e)}
+        onWheel={e => this._maybePreventScroll(e)}
+        style={{
+          ...styles.initial,
+          ...resolvedStyle
+        }}
+      >
+        {children ? (
+          <View
+            _className='ScrollViewContentContainer'
+            children={children}
+            style={{
+              ...styles.contentContainerStyleInitial,
+              ...resolvedContentContainerStyle,
+              ...(horizontal && styles.contentContainerStyleHorizontal)
+            }}
+          />
+        ) : null}
+      </View>
     )
   }
 }
