804132ce366703814cdbe65eadc49777e2f5a59e
Nicolas Gallagher <nicolasgallagher@gmail.com>
2015-12-26 14:22:36 +0000

    [fix] 'process.env.NODE_ENV' check
    Use babel to transpile the source code without bundling it.
    Use webpack to create a standalone, productionized UMD bundle.
    
    Fix #50

--- config/webpack.config.example.js
+++ config/webpack.config.example.js
@@ -1,17 +1,31 @@
-var assign = require('object-assign')
-var base = require('./webpack.config.base')
 var constants = require('./constants')
-var path = require('path')
+var webpack = require('webpack')
 
-module.exports = assign({}, base, {
+module.exports = {
   devServer: {
     contentBase: constants.EXAMPLES_DIRECTORY
   },
   entry: {
-    example: path.join(constants.EXAMPLES_DIRECTORY, 'index')
+    example: constants.EXAMPLES_DIRECTORY
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { cacheDirectory: true }
+      }
+    ]
   },
   output: {
-    filename: 'examples.js',
-    path: constants.DIST_DIRECTORY
-  }
-})
+    filename: 'examples.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+}
---
+++ config/webpack.config.js
@@ -0,0 +1,31 @@
+var constants = require('./constants')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    main: constants.DIST_DIRECTORY
+  },
+  externals: [{
+    'react': true,
+    'react-dom': true
+  }],
+  output: {
+    filename: 'react-native-web.js',
+    library: 'ReactNativeWeb',
+    libraryTarget: 'umd',
+    path: constants.DIST_DIRECTORY
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': 'production' }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        dead_code: true,
+        drop_console: true,
+        screw_ie8: true,
+        warnings: true
+      }
+    })
+  ]
+}
