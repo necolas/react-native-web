b4a9177ce3c30abcfc9d673fe90297f8a8000f35
Nicolas Gallagher <nicolasgallagher@gmail.com>
2016-03-15 13:44:50 -0700

    [add] dismissKeyboard module

---
+++ src/modules/dismissKeyboard/index.js
@@ -0,0 +1,7 @@
+import TextInputState from '../../components/TextInput/TextInputState'
+
+const dismissKeyboard = () => {
+  TextInputState.blurTextInput(TextInputState.currentlyFocusedField())
+}
+
+module.exports = dismissKeyboard
