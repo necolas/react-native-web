62d1a0f83d7403f68f5ebd511ccfbaa16a07ba34
Nicolas Gallagher <nicolasgallagher@gmail.com>
2016-03-16 00:47:53 -0700

    Add Game2048 example

--- examples/index.js
+++ examples/index.js
@@ -1,23 +1,7 @@
-import { MediaProvider, matchMedia } from 'react-media-queries'
-import App from './components/App'
-import createGetter from 'react-media-queries/lib/createMediaQueryGetter'
-import createListener from 'react-media-queries/lib/createMediaQueryListener'
 import React, { AppRegistry } from 'react-native'
+import Game2048 from './2048/Game2048'
+import TicTacToeApp from './TicTacToe/TicTacToe'
 
-const mediaQueries = {
-  small: '(min-width: 300px)',
-  medium: '(min-width: 400px)',
-  large: '(min-width: 500px)'
-}
-const ResponsiveApp = matchMedia()(App)
-const WrappedApp = () => (
-  <MediaProvider getMedia={createGetter(mediaQueries)} listener={createListener(mediaQueries)}>
-    <ResponsiveApp />
-  </MediaProvider>
-)
-
-AppRegistry.registerComponent('Example', () => WrappedApp)
-
-AppRegistry.runApplication('Example', {
+AppRegistry.runApplication('Game2048', {
   rootTag: document.getElementById('react-root')
 })
