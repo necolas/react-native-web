08b111322cf83f203765da3aef7bd7668c60a675
Jimmy <hfter789@users.noreply.github.com>
2016-12-22 09:11:40 -0800

    Picker dropdown (#1)
    Picker

---
+++ src/components/Picker/PickerItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PICKER_ITEM_PROPTYPES } from './proptypes';
+
+export default class PickerItem extends React.Component {
+
+  static propTypes = PICKER_ITEM_PROPTYPES;
+
+  render() {
+    return null;
+  }
+}
---
+++ src/components/Picker/constants.js
@@ -0,0 +1,2 @@
+export const MODE_DIALOG = 'dialog';
+export const MODE_DROPDOWN = 'dropdown';
