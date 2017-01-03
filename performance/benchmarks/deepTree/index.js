236121e32c36571dce5469d763c5f7ee5ecc4f05
Nicolas Gallagher <nicolasgallagher@gmail.com>
2017-01-02 22:46:38 -0800

    Add unregistered styles benchmark

--- performance/benchmarks/deepTree/index.js
+++ performance/benchmarks/deepTree/index.js
@@ -4,16 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactNative from 'react-native';
 
-// React Native for Web implementation of the tree
-const DeepTree = createDeepTree(ReactNative);
+const deepTreeBenchmark = ({ breadth, depth, registerStyles = true, runs, wrap }, node) => () => {
+  // React Native for Web implementation of the tree
+  const DeepTree = createDeepTree(ReactNative, { registerStyles });
 
-const deepTreeBenchmark = ({ breadth, depth, wrap, runs }, node) => () => {
-  const setup = () => { };
+  const setup = () => {};
   const teardown = () => ReactDOM.unmountComponentAtNode(node);
 
   return benchmark({
-    name: 'DeepTree',
-    description: `depth=${depth}, breadth=${breadth}, wrap=${wrap})`,
+    name: `DeepTree (${registerStyles ? 'registered' : 'unregistered'}) styles)`,
+    description: `depth=${depth}, breadth=${breadth}, wrap=${wrap}`,
     runs,
     setup,
     teardown,
--- performance/index.js
+++ performance/index.js
@@ -1,11 +1,16 @@
+import createDeepTree from './benchmarks/deepTree/createDeepTree';
 import deepTree from './benchmarks/deepTree';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactNative from 'react-native';
 
 const node = document.querySelector('.root');
+const DeepTree = createDeepTree(ReactNative);
 
 Promise.resolve()
+  .then(deepTree({ wrap: 4, depth: 3, breadth: 10, runs: 10, registerStyles: false }, node))
+  .then(deepTree({ wrap: 1, depth: 5, breadth: 3, runs: 20, registerStyles: false }, node))
   .then(deepTree({ wrap: 4, depth: 3, breadth: 10, runs: 10 }, node))
   .then(deepTree({ wrap: 1, depth: 5, breadth: 3, runs: 20 }, node))
-  .then(() => ReactDOM.render(<div>Complete</div>, node));
+  .then(() => ReactDOM.render(<DeepTree breadth={3} depth={5} id={0} wrap={1} />, node));
 
