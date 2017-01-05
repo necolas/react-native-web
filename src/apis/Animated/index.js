214d862e61d87a31133619d4f721c3c8501910da
Nicolas Gallagher <nicolasgallagher@gmail.com>
2017-01-04 17:32:34 -0800

    [add] ScrollView to Animated

--- src/apis/Animated/index.js
+++ src/apis/Animated/index.js
@@ -1,5 +1,6 @@
 import Animated from 'animated';
 import Image from '../../components/Image';
+import ScrollView from '../../components/ScrollView';
 import StyleSheet from '../StyleSheet';
 import Text from '../../components/Text';
 import View from '../../components/View';
@@ -9,6 +10,7 @@ Animated.inject.FlattenStyle(StyleSheet.flatten);
 module.exports = {
   ...Animated,
   Image: Animated.createAnimatedComponent(Image),
+  ScrollView: Animated.createAnimatedComponent(ScrollView),
   Text: Animated.createAnimatedComponent(Text),
   View: Animated.createAnimatedComponent(View)
 };
