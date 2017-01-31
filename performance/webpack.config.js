6bf81fe2b6d5151a42e6436e70735cb0bed4a8c6
Joe Tagliaferro <j.a.tagliaferro@gmail.com>
2017-01-30 23:55:05 -0500

    Fixes benchmark webpack for babel alias

--- performance/webpack.config.js
+++ performance/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    performance: './index'
+    performance: './performance/index'
   },
   output: {
     path: path.resolve(__dirname, '../dist-performance'),
