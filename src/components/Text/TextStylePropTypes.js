a2903f9d30a11265c3f8f04b1392e7bfe802fa97
Nicolas Gallagher <nicolasgallagher@gmail.com>
2017-02-05 16:37:54 -0800

    Move TextPropTypes to TextStylePropTypes

--- src/components/Text/TextStylePropTypes.js
+++ src/components/Text/TextStylePropTypes.js
@@ -1,7 +1,45 @@
-import TextPropTypes from '../../propTypes/TextPropTypes';
+import ColorPropType from '../../propTypes/ColorPropType';
+import { PropTypes } from 'react';
 import ViewStylePropTypes from '../View/ViewStylePropTypes';
 
-module.exports = process.env.NODE_ENV !== 'production' ? {
+const { number, oneOf, oneOfType, shape, string } = PropTypes;
+const numberOrString = oneOfType([ number, string ]);
+
+const ShadowOffsetPropType = shape({ width: number, height: number });
+const TextAlignPropType = oneOf([ 'center', 'inherit', 'justify', 'justify-all', 'left', 'right' ]);
+const WritingDirectionPropType = oneOf([ 'auto', 'ltr', 'rtl' ]);
+
+const TextOnlyStylePropTypes = {
+  color: ColorPropType,
+  fontFamily: string,
+  fontSize: numberOrString,
+  fontStyle: string,
+  fontWeight: string,
+  letterSpacing: numberOrString,
+  lineHeight: numberOrString,
+  textAlign: TextAlignPropType,
+  textAlignVertical: oneOf([ 'auto', 'bottom', 'center', 'top' ]),
+  textDecorationLine: string,
+  textShadowColor: ColorPropType,
+  textShadowOffset: ShadowOffsetPropType,
+  textShadowRadius: number,
+  writingDirection: WritingDirectionPropType,
+  /* @platform web */
+  textOverflow: string,
+  textRendering: oneOf([ 'auto', 'geometricPrecision', 'optimizeLegibility', 'optimizeSpeed' ]),
+  textTransform: oneOf([ 'capitalize', 'lowercase', 'none', 'uppercase' ]),
+  unicodeBidi: oneOf([ 'normal', 'bidi-override', 'embed', 'isolate', 'isolate-override', 'plaintext' ]),
+  whiteSpace: string,
+  wordWrap: string,
+  MozOsxFontSmoothing: string,
+  WebkitFontSmoothing: string,
+  // opt-out of RTL flipping
+  textAlign$noI18n: TextAlignPropType,
+  textShadowOffset$noI18n: ShadowOffsetPropType,
+  writingDirection$noI18n: WritingDirectionPropType
+};
+
+module.exports = {
   ...ViewStylePropTypes,
-  ...TextPropTypes
-} : {};
+  ...TextOnlyStylePropTypes
+};
