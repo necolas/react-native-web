95b28e9cc87fd2135e2be85d408db3ab2322a967
Drew Wells <drew.wells00@gmail.com>
2017-02-10 13:44:36 -0600

    basic end 2 end example of setting up react-native-web
    - ignore upstream babel
    - add starter page
    - add react and babel-react support

---
+++ examples/souptonuts/README.md
@@ -0,0 +1,16 @@
+Install webpack-dev-server and dependencies
+
+```
+npm i -g webpack-dev-server
+npm i
+```
+
+Run and open site
+```
+webpack-dev-server
+open http://localhost:8080
+```
+
+You should see this:
+
+![RN Welcome Screen](welcome.png)
---
+++ examples/souptonuts/index.web.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class ReactNativeWeb extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.web.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeWeb', () => ReactNativeWeb);
+AppRegistry.runApplication('ReactNativeWeb', { rootTag: document.getElementById('react-app') });
