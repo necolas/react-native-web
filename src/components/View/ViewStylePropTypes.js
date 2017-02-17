bb417900a9f6d09a5fcd3f8c07505632ddb24549
Nathan Tran <ntran013@gmail.com>
2017-02-17 21:52:27 +1100

    [add] `willChange` style prop type
    Close #367

--- src/components/View/ViewStylePropTypes.js
+++ src/components/View/ViewStylePropTypes.js
@@ -45,5 +45,6 @@ module.exports = {
   transitionTimingFunction: string,
   userSelect: string,
   visibility: hiddenOrVisible,
-  WebkitOverflowScrolling: oneOf([ 'auto', 'touch' ])
+  WebkitOverflowScrolling: oneOf([ 'auto', 'touch' ]),
+  willChange: string
 };
