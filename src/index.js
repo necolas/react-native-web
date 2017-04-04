9c2a21ccbfc7e62104e759e2203bb406837aa0ad
Jhen <developer@jhen.me>
2017-04-04 19:40:25 +0800

    Add Modal component

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ import ActivityIndicator from './components/ActivityIndicator';
 import Button from './components/Button';
 import Image from './components/Image';
 import ListView from './components/ListView';
+import Modal from './components/Modal';
 import ProgressBar from './components/ProgressBar';
 import ScrollView from './components/ScrollView';
 import Switch from './components/Switch';
@@ -85,6 +86,7 @@ const ReactNative = {
   Button,
   Image,
   ListView,
+  Modal,
   ProgressBar,
   ScrollView,
   Switch,
