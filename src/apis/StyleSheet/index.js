5a5eb5425f2d5ee1e7f15d923b580f7d0027530e
Nicolas Gallagher <nicolasgallagher@gmail.com>
2017-06-15 19:30:18 -0700

    Allow component styles to be editable in React Dev Tools

--- src/apis/StyleSheet/index.js
+++ src/apis/StyleSheet/index.js
@@ -1,6 +1,14 @@
 import flattenStyle from './flattenStyle';
 import StyleRegistry from './registry';
 
+// allow component styles to be editable in React Dev Tools
+if (process.env.NODE_ENV !== 'production') {
+  const { canUseDOM } = require('fbjs/lib/ExecutionEnvironment');
+  if (canUseDOM && window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__.resolveRNStyle = flattenStyle;
+  }
+}
+
 const absoluteFillObject = {
   position: 'absolute',
   left: 0,
