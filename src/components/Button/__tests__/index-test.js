5a379ce67b97e262e88cff7182e53c7c08ac2603
Andrew Kennedy <akennedy2292@gmail.com>
2017-07-30 22:34:17 -0700

    add tests for button component

--- src/components/Button/__tests__/index-test.js
+++ src/components/Button/__tests__/index-test.js
@@ -1,5 +1,48 @@
 /* eslint-env jasmine, jest */
 
+import React from 'react';
+import Button from '..';
+import { mount, shallow } from 'enzyme';
+
+const findNativeButton = wrapper => wrapper.find('[role="button"]');
+
 describe('components/Button', () => {
-  test.skip('NO TEST COVERAGE', () => {});
+  describe('rendered element', () => {
+    test('is a "div"', () => {
+      const onPress = () => {}
+      const button = findNativeButton(mount(<Button onPress={onPress} title="" />));
+      expect(button.type()).toBe('div');
+    });
+
+    test('can display text', () => {
+      const onPress = () => {}
+      const text = 'Click me';
+      const component = mount(<Button onPress={onPress} title={text} />);
+      expect(component.text()).toEqual(text);
+    });
+  });
+
+  test('prop "color"', () => {
+    const onPress = () => {}
+    const color = 'blue';
+    const button = findNativeButton(mount(<Button color={color} onPress={onPress} title="" />));
+    expect(button.prop('style')).toEqual({backgroundColor: color});
+  });
+
+  test('prop "disabled"', () => {
+    const onPress = () => {}
+    // true
+    let component = shallow(<Button disabled={true} onPress={onPress} title="" />);
+    expect(component.prop('disabled')).toEqual(true);
+    // false
+    component = shallow(<Button disabled={false} onPress={onPress} title="" />);
+    expect(component.prop('disabled')).toEqual(false);
+  });
+
+  test('prop "onPress"', () => {
+    const onPress = jest.fn();
+    const component = shallow(<Button onPress={onPress} title="" />);
+    component.simulate('press');
+    expect(onPress).toHaveBeenCalled();
+  });
 });
