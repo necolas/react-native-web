b23a4f55dc1571b1e5e99c7d84aadd034df5177b
Unknown <vincebt06@gmail.com>
2017-09-07 01:22:55 +0200

    [add] support for Keyboard API
    Close #625

---
+++ src/apis/Keyboard/index.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2016-present, Nicolas Gallagher.
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @providesModule Keyboard
+ * @flow
+ */
+
+import dismissKeyboard from '../../modules/dismissKeyboard';
+
+const Keyboard = {
+  addListener() {},
+  dismiss() {
+    dismissKeyboard();
+  },
+  removeAllListeners() {},
+  removeListener() {}
+};
+
+export default Keyboard;
