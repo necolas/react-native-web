0afc23e47759f17d502e60e004f4aa4efb4eae4f
Unknown <vincebt06@gmail.com>
2017-09-17 02:10:32 +0200

    Basic support for RefreshControl

--- src/components/RefreshControl/index.js
+++ src/components/RefreshControl/index.js
@@ -1,2 +1,55 @@
-import UnimplementedView from '../UnimplementedView';
-export default UnimplementedView;
+/**
+ * Copyright (c) 2017-present, Nicolas Gallagher.
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @providesModule RefreshControl
+ * @flow
+ */
+
+import View from '../View';
+import ViewPropTypes from '../View/ViewPropTypes';
+import ColorPropType from '../../propTypes/ColorPropType';
+import { arrayOf, bool, func, number, oneOf, string } from 'prop-types';
+import React, { Component } from 'react';
+
+class RefreshControl extends Component {
+  static propTypes = {
+    ...ViewPropTypes,
+    colors: arrayOf(ColorPropType),
+    enabled: bool,
+    onRefresh: func,
+    progressBackgroundColor: ColorPropType,
+    progressViewOffset: number,
+    refreshing: bool.isRequired,
+    size: oneOf([0, 1]),
+    tintColor: ColorPropType,
+    title: string,
+    titleColor: ColorPropType,
+  };
+
+  render() {
+    const {
+      /* eslint-disable */
+      colors,
+      enabled,
+      onRefresh,
+      progressBackgroundColor,
+      progressViewOffset,
+      refreshing,
+      size,
+      tintColor,
+      title,
+      titleColor,
+      /* eslint-enable */
+      ...rest
+    } = this.props;
+
+    return <View {...rest} />;
+  }
+}
+
+export default RefreshControl;
