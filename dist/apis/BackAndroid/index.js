71bf9dc82c47591b4e8320a6df09ccf7fa27d0c4
289420843@qq.com <289420843@qq.com>
2017-09-18 16:39:40 +0800

    新增Modal DeviceEventEmitter

---
+++ dist/apis/BackAndroid/index.js
@@ -0,0 +1,1 @@
+Object.defineProperty(exports,"__esModule",{value:true});function emptyFunction(){}var BackAndroid={exitApp:emptyFunction,addEventListener:function addEventListener(){return{remove:emptyFunction};},removeEventListener:emptyFunction};exports.default=BackAndroid;
---
+++ dist/modules/AssetRegistry/index.js
@@ -0,0 +1,1 @@
+Object.defineProperty(exports,"__esModule",{value:true});exports.registerAsset=registerAsset;exports.getAssetByID=getAssetByID;var assets=[];function registerAsset(asset){return assets.push(asset);}function getAssetByID(assetId){return assets[assetId-1];}
