6942e4e41709651a5b0ee4093192de4a3537dd75
Nicolas Gallagher <nicolasgallagher@gmail.com>
2017-09-18 19:47:03 -0700

    Shallow render ActivityIndicator tests
    These snapshots should be improved by flattening the React Native style
    object.

--- src/components/ActivityIndicator/__tests__/index-test.js
+++ src/components/ActivityIndicator/__tests__/index-test.js
@@ -2,46 +2,46 @@
 
 import ActivityIndicator from '..';
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 describe('components/ActivityIndicator', () => {
   describe('prop "animating"', () => {
     test('is "true"', () => {
-      const component = render(<ActivityIndicator animating={true} />);
+      const component = shallow(<ActivityIndicator animating={true} />);
       expect(component).toMatchSnapshot();
     });
 
     test('is "false"', () => {
-      const component = render(<ActivityIndicator animating={false} />);
+      const component = shallow(<ActivityIndicator animating={false} />);
       expect(component).toMatchSnapshot();
     });
   });
 
   test('prop "color"', () => {
-    const component = render(<ActivityIndicator color="red" />);
+    const component = shallow(<ActivityIndicator color="red" />).find('svg');
     expect(component).toMatchSnapshot();
   });
 
   describe('prop "hidesWhenStopped"', () => {
     test('is "true"', () => {
-      const component = render(<ActivityIndicator animating={false} hidesWhenStopped={true} />);
+      const component = shallow(<ActivityIndicator animating={false} hidesWhenStopped={true} />);
       expect(component).toMatchSnapshot();
     });
 
     test('is "false"', () => {
-      const component = render(<ActivityIndicator animating={false} hidesWhenStopped={false} />);
+      const component = shallow(<ActivityIndicator animating={false} hidesWhenStopped={false} />);
       expect(component).toMatchSnapshot();
     });
   });
 
   describe('prop "size"', () => {
     test('is "large"', () => {
-      const component = render(<ActivityIndicator size="large" />);
+      const component = shallow(<ActivityIndicator size="large" />);
       expect(component).toMatchSnapshot();
     });
 
     test('is a number', () => {
-      const component = render(<ActivityIndicator size={30} />);
+      const component = shallow(<ActivityIndicator size={30} />);
       expect(component).toMatchSnapshot();
     });
   });
