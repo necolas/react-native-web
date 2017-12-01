b1e860ab40d634aab50c2bdec5f8e2091dbc392d
Nicolas Gallagher <nicolasgallagher@gmail.com>
2017-12-01 15:21:00 -0800

    Add ImageBackground docs

---
+++ docs/storybook/1-components/Image/ImageBackgroundScreen.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/jsx-sort-props */
+
+/**
+ * @flow
+ */
+
+import React from 'react';
+import PropChildren from './examples/PropChildren';
+import UIExplorer, {
+  AppText,
+  Code,
+  Description,
+  DocItem,
+  Section,
+  storiesOf
+} from '../../ui-explorer';
+
+const ImageBackgroundScreen = () => (
+  <UIExplorer title="ImageBackground" url="1-components/ImageBackground">
+    <Description>
+      A image component with support for child content.
+    </Description>
+
+    <Section title="Props">
+      <DocItem name="...Image props" />
+
+      <DocItem
+        name="children"
+        typeInfo="?any"
+        description="Content to display over the image."
+        example={{
+          render: () => <PropChildren />
+        }}
+      />
+
+      <DocItem name="imageStyle" typeInfo="?style" description="Styles for the inner image." />
+    </Section>
+  </UIExplorer>
+);
+
+storiesOf('Components', module).add('ImageBackground', ImageBackgroundScreen);
