6d9154196e510219d5b39e5d2bebca7021d50b89
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-01-04 13:41:49 -0800

    [fix] StyleSheet.hairlineWidth guard against missing document.body

--- packages/react-native-web/src/exports/StyleSheet/getHairlineWidth.js
+++ packages/react-native-web/src/exports/StyleSheet/getHairlineWidth.js
@@ -8,13 +8,16 @@ import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 const getHairlineWidth = () => {
   let hairlineWidth = 1;
   if (canUseDOM && window.devicePixelRatio && window.devicePixelRatio >= 2) {
-    const node = document.createElement('div');
-    node.style.border = '.5px solid transparent';
-    document.body.appendChild(node);
-    if (node.offsetHeight === 1) {
-      hairlineWidth = 0.5;
+    const body = document.body;
+    if (body) {
+      const node = document.createElement('div');
+      node.style.border = '.5px solid transparent';
+      body.appendChild(node);
+      if (node.offsetHeight === 1) {
+        hairlineWidth = 0.5;
+      }
+      body.removeChild(node);
     }
-    document.body.removeChild(node);
   }
   return hairlineWidth;
 };
