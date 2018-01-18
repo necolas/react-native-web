e9cab3534a2f71a036d7bafc754414b3e1c42845
lttb <kenzhaev.artur@gmail.com>
2018-01-19 01:37:09 +0300

    Add styled-jss to benchmarks

---
+++ packages/benchmarks/src/implementations/styled-jss-core/View.js
@@ -0,0 +1,19 @@
+import styled from './styled';
+
+const View = styled('div')({
+  'align-items': 'stretch',
+  'border-width': '0px',
+  'border-style': 'solid',
+  'box-sizing': 'border-box',
+  display: 'flex',
+  'flex-basis': 'auto',
+  'flex-direction': 'column',
+  'flex-shrink': '0',
+  margin: '0px',
+  padding: '0x',
+  position: 'relative',
+  'min-height': '0px',
+  'min-width': '0px'
+});
+
+export default View;
