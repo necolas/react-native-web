f95d98cfc16a7c33e973a45f95e77398dc506237
Ron Arts <ron.arts@novantum.com>
2018-02-18 13:22:04 +0100

    Initial SectionList support, works with the Expo tabs template

--- scripts/jest/config.js
+++ scripts/jest/config.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = {
+  globals: {
+    __DEV__: false
+  },
   rootDir: process.cwd(),
   roots: ['<rootDir>/packages'],
   setupFiles: ['raf/polyfill'],
+  modulePathIgnorePatterns: ['<rootDir>/packages/react-native-web/dist'],
   setupTestFrameworkScriptFile: require.resolve('./setupFramework.js'),
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testEnvironment: 'jsdom',
