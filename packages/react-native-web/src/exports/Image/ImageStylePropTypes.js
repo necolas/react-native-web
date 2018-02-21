306cf679327772d5939e1334617f1c1fa7454e3c
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-02-20 17:45:19 -0800

    [add] Image style support for animations, interactions, and filters

--- packages/react-native-web/src/exports/Image/ImageStylePropTypes.js
+++ packages/react-native-web/src/exports/Image/ImageStylePropTypes.js
@@ -2,16 +2,20 @@
  * @flow
  */
 
+import AnimationPropTypes from '../../modules/AnimationPropTypes';
 import BorderPropTypes from '../../modules/BorderPropTypes';
 import ColorPropType from '../ColorPropType';
 import ImageResizeMode from './ImageResizeMode';
+import InteractionPropTypes from '../../modules/InteractionPropTypes';
 import LayoutPropTypes from '../../modules/LayoutPropTypes';
 import ShadowPropTypes from '../../modules/ShadowPropTypes';
 import TransformPropTypes from '../../modules/TransformPropTypes';
 import { number, oneOf, string } from 'prop-types';
 
 const ImageStylePropTypes = {
+  ...AnimationPropTypes,
   ...BorderPropTypes,
+  ...InteractionPropTypes,
   ...LayoutPropTypes,
   ...ShadowPropTypes,
   ...TransformPropTypes,
@@ -26,7 +30,8 @@ const ImageStylePropTypes = {
   /**
    * @platform web
    */
-  boxShadow: string
+  boxShadow: string,
+  filter: string
 };
 
 export default ImageStylePropTypes;
