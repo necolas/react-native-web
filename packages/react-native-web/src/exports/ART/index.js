e386e3d317486d87b99ed383cb8e627a23a5e76f
Theo Seguier <tseguier@student.42.fr>
2018-02-27 10:39:54 -0800

    Update index.js

--- packages/react-native-web/src/exports/ART/index.js
+++ packages/react-native-web/src/exports/ART/index.js
@@ -1,11 +1,4 @@
 /**
- * @Author: Theo Seguier <key>
- * @Date:   2018-02-27T10:16:16-08:00
- * @Last modified by:   key
- * @Last modified time: 2018-02-27T10:30:12-08:00
- */
-
-/**
  * Copyright (c) 2015-present, Nicolas Gallagher.
  * Copyright (c) 2015-present, Facebook, Inc.
  *
