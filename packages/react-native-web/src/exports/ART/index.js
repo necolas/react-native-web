5f57c30e7fbbb96da555d86c489966aa2d9aa34f
anonkey <theo.seguier@wanadoo.fr>
2018-02-27 10:32:18 -0800

    [fix] Import react-art only client side to avoid crash

--- packages/react-native-web/src/exports/ART/index.js
+++ packages/react-native-web/src/exports/ART/index.js
@@ -1,2 +1,22 @@
-import ART from 'react-art';
+/**
+ * @Author: Theo Seguier <key>
+ * @Date:   2018-02-27T10:16:16-08:00
+ * @Last modified by:   key
+ * @Last modified time: 2018-02-27T10:30:12-08:00
+ */
+
+/**
+ * Copyright (c) 2015-present, Nicolas Gallagher.
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @providesModule Dimensions
+ * @flow
+ */
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
+
+const ART = canUseDOM ? require('react-art') : null;
+
 export default ART;
