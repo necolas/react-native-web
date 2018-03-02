acf6e66806d82026c5bdb63788715b938d7a2666
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-03-02 23:31:25 +0100

    Fire 'onLayout' when elements are resized

---
+++ website/storybook/1-components/Text/examples/onLayout.js
@@ -0,0 +1,20 @@
+/**
+ * @flow
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class OnLayoutExample extends React.Component {
+  state = {
+    layoutInfo: {}
+  };
+
+  onLayout = ({ nativeEvent }) => {
+    this.setState({ layoutInfo: nativeEvent.layout });
+  };
+
+  render() {
+    return <Text onLayout={this.onLayout}>{JSON.stringify(this.state.layoutInfo)}</Text>;
+  }
+}
--- website/storybook/1-components/View/ViewScreen.js
+++ website/storybook/1-components/View/ViewScreen.js
@@ -4,6 +4,7 @@
  * @flow
  */
 
+import OnLayoutExample from './examples/onLayout';
 import PropPointerEvents from './examples/PropPointerEvents';
 import transformExamples from './examples/transforms';
 import ZIndexExample from './examples/ZIndex';
@@ -125,13 +126,20 @@ const ViewScreen = () => (
       <DocItem
         name="onLayout"
         typeInfo="?function"
-        description={
+        description={[
           <AppText>
             Invoked on mount and layout changes with{' '}
             <Code>{'{ nativeEvent: { layout: { x, y, width, height } } }'}</Code>, where{' '}
             <Code>x</Code> and <Code>y</Code> are the offsets from the parent node.
+          </AppText>,
+          <AppText>
+            NOTE: Behind the hood React Native for Web uses <Code>ResizeObserver</Code> and doesn't
+            polyfill it when not supported.
           </AppText>
-        }
+        ]}
+        example={{
+          render: () => <OnLayoutExample />
+        }}
       />
 
       <DocItem
