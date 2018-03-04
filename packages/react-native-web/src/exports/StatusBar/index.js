42ac42d307aab5357678d188254d4a5e5221fbbf
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-03-04 04:45:44 +0100

    [StatusBar] implemented all static methods

--- packages/react-native-web/src/exports/StatusBar/index.js
+++ packages/react-native-web/src/exports/StatusBar/index.js
@@ -7,17 +7,77 @@
  * @providesModule StatusBar
  * @flow
  */
-
 import { Component } from 'react';
+
+
+const {head} = document
+
+let _barStyle = 'default'
+let _hidden = false
+let _translucent = false
+
+
+function getMetaTag(attrName) {
+  const htmlCollection = head.getElementsByTagName('meta')
+
+  let tag = Array.from(htmlCollection).filter(({name}) => name === attrName)[0]
+
+  if(!tag) {
+    tag = document.createElement('meta')
+    tag.name = attrName
+
+    head.appendChild(tag)
+  }
+
+  return tag
+}
+
+function setAppleMobileWebAppCapable() {
+  getMetaTag('apple-mobile-web-app-capable').content =
+    (_hidden || _translucent || _barStyle !== 'default') ? 'yes' : 'no'
+}
+
+function setAppleMobileWebAppStatusBarStyle() {
+  setAppleMobileWebAppCapable()
+
+  getMetaTag('apple-mobile-web-app-status-bar-style').content =
+    _translucent ? 'black-translucent' : _barStyle
+}
+
 
 export default class StatusBar extends Component<*> {
   static currentHeight = 0
 
-  static setBackgroundColor() {}
-  static setBarStyle() {}
-  static setHidden() {}
+
+  static setBackgroundColor(color, animated) {
+    getMetaTag('theme-color').content = color
+  }
+
+  static setBarStyle(style, animated) {
+    if(style === 'light-content') style = 'black'
+
+    if(!['black', 'black-translucent', 'default'].includes(style)) return
+
+    _barStyle = style
+
+    setAppleMobileWebAppStatusBarStyle()
+  }
+
+  static setHidden(hidden, animation) {
+    _hidden = hidden
+
+    setAppleMobileWebAppCapable()
+  }
+
   static setNetworkActivityIndicatorVisible() {}
-  static setTranslucent() {}
+
+  static setTranslucent(translucent) {
+    _translucent = translucent
+
+    setAppleMobileWebAppStatusBarStyle()
+  }
+
+
   render() {
     return null;
   }
