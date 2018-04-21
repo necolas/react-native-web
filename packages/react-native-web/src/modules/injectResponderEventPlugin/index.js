1a85a5199be9e29e1ec726bd1144f09e30b65ae5
Philipp Spiess <hello@philippspiess.com>
2018-04-21 13:29:45 +0200

    Prepare for number top level events

--- packages/react-native-web/src/modules/injectResponderEventPlugin/index.js
+++ packages/react-native-web/src/modules/injectResponderEventPlugin/index.js
@@ -7,44 +7,12 @@ import ReactDOMUnstableNativeDependencies from 'react-dom/unstable-native-depend
 const { EventPluginHub } = ReactDOM.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 const { ResponderEventPlugin, ResponderTouchHistoryStore } = ReactDOMUnstableNativeDependencies;
 
-const topMouseDown = 'topMouseDown';
-const topMouseMove = 'topMouseMove';
-const topMouseUp = 'topMouseUp';
-const topScroll = 'topScroll';
-const topSelectionChange = 'topSelectionChange';
-const topTouchCancel = 'topTouchCancel';
-const topTouchEnd = 'topTouchEnd';
-const topTouchMove = 'topTouchMove';
-const topTouchStart = 'topTouchStart';
-
-const endDependencies = [topTouchCancel, topTouchEnd, topMouseUp];
-const moveDependencies = [topTouchMove, topMouseMove];
-const startDependencies = [topTouchStart, topMouseDown];
-
-/**
- * Setup ResponderEventPlugin dependencies
- */
-ResponderEventPlugin.eventTypes.responderMove.dependencies = moveDependencies;
-ResponderEventPlugin.eventTypes.responderEnd.dependencies = endDependencies;
-ResponderEventPlugin.eventTypes.responderStart.dependencies = startDependencies;
-ResponderEventPlugin.eventTypes.responderRelease.dependencies = endDependencies;
-ResponderEventPlugin.eventTypes.responderTerminationRequest.dependencies = [];
-ResponderEventPlugin.eventTypes.responderGrant.dependencies = [];
-ResponderEventPlugin.eventTypes.responderReject.dependencies = [];
-ResponderEventPlugin.eventTypes.responderTerminate.dependencies = [];
-ResponderEventPlugin.eventTypes.moveShouldSetResponder.dependencies = moveDependencies;
-ResponderEventPlugin.eventTypes.selectionChangeShouldSetResponder.dependencies = [
-  topSelectionChange
-];
-ResponderEventPlugin.eventTypes.scrollShouldSetResponder.dependencies = [topScroll];
-ResponderEventPlugin.eventTypes.startShouldSetResponder.dependencies = startDependencies;
-
 const originalExtractEvents = ResponderEventPlugin.extractEvents;
 ResponderEventPlugin.extractEvents = (topLevelType, targetInst, nativeEvent, nativeEventTarget) => {
   const hasActiveTouches = ResponderTouchHistoryStore.touchHistory.numberActiveTouches > 0;
   if (
     // Filter out mousemove and mouseup events when a touch hasn't started yet
-    ((topLevelType === topMouseMove || topLevelType === topMouseUp) && !hasActiveTouches) ||
+    ((nativeEvent.type === 'mousemove' || nativeEvent.type === 'mouseup') && !hasActiveTouches) ||
     // Filter out events from wheel/middle and right click.
     (nativeEvent.button === 1 || nativeEvent.button === 2)
   ) {
