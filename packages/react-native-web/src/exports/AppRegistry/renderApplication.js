f610994bef27b67d1031d106e1ef8f28c1ab947e
Jam Risser <jam@codejam.ninja>
2018-04-28 19:23:48 +0200

    Always hydrate

--- packages/react-native-web/src/exports/AppRegistry/renderApplication.js
+++ packages/react-native-web/src/exports/AppRegistry/renderApplication.js
@@ -11,11 +11,8 @@
 import AppContainer from './AppContainer';
 import invariant from 'fbjs/lib/invariant';
 import hydrate from '../../modules/hydrate';
-import render from '../render';
 import styleResolver from '../StyleSheet/styleResolver';
 import React, { type ComponentType } from 'react';
-
-const renderFn = process.env.NODE_ENV !== 'production' ? render : hydrate;
 
 export default function renderApplication<Props: Object>(
   RootComponent: ComponentType<Props>,
@@ -26,7 +23,7 @@ export default function renderApplication<Props: Object>(
 ) {
   invariant(rootTag, 'Expect to have a valid rootTag, instead got ', rootTag);
 
-  renderFn(
+  hydrate(
     <AppContainer WrapperComponent={WrapperComponent} rootTag={rootTag}>
       <RootComponent {...initialProps} />
     </AppContainer>,
