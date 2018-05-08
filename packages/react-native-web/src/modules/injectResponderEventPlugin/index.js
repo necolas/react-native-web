ae1db7575c662c0d874c858f0dd0ac7432a0744f
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-05-08 11:57:26 -0700

    [change] ResponderEventPlugin filters browser emulated mouse events
    Browsers dispatch mouse events after touch events:
    https://developer.mozilla.org/en-US/docs/Web/API/Touch_events/Supporting_both_TouchEvent_and_MouseEvent
    
    There have been several attempts to avoid this behaviour affecting the
    ResponderEvent system. The previous approach of cancelling the event in
    the `onResponderRelease` event handler can end up cancelling other
    events that are expected, e.g., `focus`.
    
    Instead, this patch changes the `ResponderEventPlugin.extractEvents`
    function to filter the mouse events that occur a short time after a
    touch event. (It's assumed that people will not be clicking a mouse
    within a few hundred ms of performing a touch.) This allows the
    ResponderEvent system to function as expected and leaves other callbacks
    to fire as they would be expected to in React DOM, i.e., both
    'onTouchStart' and 'onMouseDown' will be called following a touch start.
    
    Fix #932
    Ref #802

--- packages/react-native-web/src/modules/injectResponderEventPlugin/index.js
+++ packages/react-native-web/src/modules/injectResponderEventPlugin/index.js
@@ -39,14 +39,29 @@ ResponderEventPlugin.eventTypes.selectionChangeShouldSetResponder.dependencies =
 ResponderEventPlugin.eventTypes.scrollShouldSetResponder.dependencies = [topScroll];
 ResponderEventPlugin.eventTypes.startShouldSetResponder.dependencies = startDependencies;
 
+let lastActiveTouchTimestamp = null;
+
 const originalExtractEvents = ResponderEventPlugin.extractEvents;
 ResponderEventPlugin.extractEvents = (topLevelType, targetInst, nativeEvent, nativeEventTarget) => {
   const hasActiveTouches = ResponderTouchHistoryStore.touchHistory.numberActiveTouches > 0;
+
+  let shouldSkipMouseAfterTouch = false;
+  if (nativeEvent.type.indexOf('touch') > -1) {
+    lastActiveTouchTimestamp = Date.now();
+  } else if (lastActiveTouchTimestamp && nativeEvent.type.indexOf('mouse') > -1) {
+    const now = Date.now();
+    shouldSkipMouseAfterTouch = lastActiveTouchTimestamp - now < 250;
+  }
+
   if (
     // Filter out mousemove and mouseup events when a touch hasn't started yet
     ((topLevelType === topMouseMove || topLevelType === topMouseUp) && !hasActiveTouches) ||
     // Filter out events from wheel/middle and right click.
-    (nativeEvent.button === 1 || nativeEvent.button === 2)
+    (nativeEvent.button === 1 || nativeEvent.button === 2) ||
+    // Filter out mouse events that browsers dispatch immediately after touch events end
+    // Prevents the REP from calling handlers twice for touch interactions.
+    // See #802 and #932.
+    shouldSkipMouseAfterTouch
   ) {
     return;
   }
