a71c87ab14910f2dbe024188b092012757fb9259
Sunny R Gupta <sunnykgupta@users.noreply.github.com>
2018-05-21 13:59:36 +0530

    Typo fix in example.
    Being a potter-head who JavaScripts, could not refrain myself from correcting the movie name. #nerdalert

--- website/storybook/1-components/Picker/examples/PickerExample.js
+++ website/storybook/1-components/Picker/examples/PickerExample.js
@@ -8,7 +8,7 @@ import { Picker, StyleSheet, View } from 'react-native';
 const PickerExample = props => (
   <View style={styles.root}>
     <Picker {...props}>
-      <Picker.Item label="Sourcerer's Stone" value="book-1" />
+      <Picker.Item label="Sorcerer's Stone" value="book-1" />
       <Picker.Item label="Chamber of Secrets" value="book-2" />
       <Picker.Item label="Prisoner of Azkaban" value="book-3" />
       <Picker.Item label="Goblet of Fire" value="book-4" />
