4fd3b459ca157d150f3a784e4931fa461411aaa8
Charlie Croom <ccroom@twitter.com>
2018-08-10 13:54:31 -0700

    Handle Enter/Space for additional a11y roles

--- packages/react-native-web/src/exports/createElement/__tests__/index-test.js
+++ packages/react-native-web/src/exports/createElement/__tests__/index-test.js
@@ -36,24 +36,32 @@ describe('modules/createElement', () => {
       expect(component.find('div').length).toBe(1);
     });
 
-    [{ disabled: true }, { disabled: false }].forEach(({ disabled }) => {
-      describe(`value is "button" and disabled is "${disabled}"`, () => {
-        [{ name: 'Enter', which: 13 }, { name: 'Space', which: 32 }].forEach(({ name, which }) => {
-          test(`"onClick" is ${disabled ? 'not ' : ''}called when "${name}" is pressed`, () => {
-            const onClick = jest.fn();
-            const component = shallow(
-              createElement('span', { accessibilityRole: 'button', disabled, onClick })
+    [{ accessibilityRole: 'button' }, { accessibilityRole: 'menuitem' }].forEach(
+      ({ accessibilityRole }) => {
+        [{ disabled: true }, { disabled: false }].forEach(({ disabled }) => {
+          describe(`value is "button" and disabled is "${disabled}"`, () => {
+            [{ name: 'Enter', which: 13 }, { name: 'Space', which: 32 }].forEach(
+              ({ name, which }) => {
+                test(`"onClick" is ${
+                  disabled ? 'not ' : ''
+                }called when "${name}" is pressed`, () => {
+                  const onClick = jest.fn();
+                  const component = shallow(
+                    createElement('span', { accessibilityRole, disabled, onClick })
+                  );
+                  component.find('span').simulate('keyPress', {
+                    isDefaultPrevented() {},
+                    nativeEvent: {},
+                    preventDefault() {},
+                    which
+                  });
+                  expect(onClick).toHaveBeenCalledTimes(disabled ? 0 : 1);
+                });
+              }
             );
-            component.find('span').simulate('keyPress', {
-              isDefaultPrevented() {},
-              nativeEvent: {},
-              preventDefault() {},
-              which
-            });
-            expect(onClick).toHaveBeenCalledTimes(disabled ? 0 : 1);
           });
         });
-      });
-    });
+      }
+    );
   });
 });
