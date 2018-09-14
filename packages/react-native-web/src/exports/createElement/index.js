815f4344ab370d85d518cd95cbc34c3d63f1ed83
johakr <Johannes.Kronmueller@gmail.com>
2018-09-14 09:47:35 +0200

    fix react-dom 16.5

--- packages/react-native-web/src/exports/createElement/index.js
+++ packages/react-native-web/src/exports/createElement/index.js
@@ -11,12 +11,10 @@ import AccessibilityUtil from '../../modules/AccessibilityUtil';
 import createDOMProps from '../../modules/createDOMProps';
 import normalizeNativeEvent from '../../modules/normalizeNativeEvent';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { injectEventPluginsByName } from 'react-dom/unstable-native-dependencies';
 import ResponderEventPlugin from '../../modules/ResponderEventPlugin';
 
-const { EventPluginHub } = ReactDOM.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-
-EventPluginHub.injection.injectEventPluginsByName({
+injectEventPluginsByName({
   ResponderEventPlugin
 });
 
