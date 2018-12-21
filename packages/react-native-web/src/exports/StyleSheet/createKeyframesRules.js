34d8160a4395f0d537d76883a92292725b3f4e8d
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-12-21 15:13:11 +0000

    [fix] CSS animation insertion for Android 5.1 HuaWei browser
    Inserting unprefixed CSS keyframes rules causes a `SYNTAX_ERR: DOM Exception
    12` error in Android 5.1. A similar issue with inserting rules containing
    vendor-prefixed pseudo-selectors was patched by wrapping rule in `@media all
    {}` blocks. This patch removes the media query wrapper from keyframe animations
    (as it doesn't prevent the error) and relies on `CSSStyleSheet::insertRule`
    being called within a try-catch block.
    
    Fix #1199
    Close #1210

--- packages/react-native-web/src/exports/StyleSheet/createKeyframesRules.js
+++ packages/react-native-web/src/exports/StyleSheet/createKeyframesRules.js
@@ -29,7 +29,7 @@ const makeSteps = keyframes =>
 const createKeyframesRules = (keyframes: Object): Array<String> => {
   const identifier = createIdentifier(keyframes);
   const rules = prefixes.map(prefix => {
-    return `@media all {@${prefix}keyframes ${identifier}{${makeSteps(keyframes)}}}`;
+    return `@${prefix}keyframes ${identifier}{${makeSteps(keyframes)}}`;
   });
   return { identifier, rules };
 };
