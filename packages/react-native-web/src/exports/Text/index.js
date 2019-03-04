06557c6be0dc05b7cc44a457b4822d6c68996e03
Michael S. Kazmier <michael.s.kazmier@uscis.dhs.gov>
2019-03-04 15:51:23 -0700

    Fix issue #1011 - adds touchable event handlers to text

--- packages/react-native-web/src/exports/Text/index.js
+++ packages/react-native-web/src/exports/Text/index.js
@@ -12,11 +12,27 @@ import applyLayout from '../../modules/applyLayout';
 import applyNativeMethods from '../../modules/applyNativeMethods';
 import { bool } from 'prop-types';
 import { Component } from 'react';
+import nullthrows from 'nullthrows';
 import createElement from '../createElement';
 import StyleSheet from '../StyleSheet';
 import TextPropTypes from './TextPropTypes';
+import Touchable from '../Touchable';
 
-class Text extends Component<*> {
+type ResponseHandlers = {|
+  onStartShouldSetResponder: () => boolean,
+  onResponderGrant: (event: *, dispatchID: string) => void,
+  onResponderMove: (event: *) => void,
+  onResponderRelease: (event: *) => void,
+  onResponderTerminate: (event: *) => void,
+  onResponderTerminationRequest: () => boolean
+|};
+
+const isTouchable = (props: *): boolean =>
+  props.onPress != null || props.onLongPress != null || props.onStartShouldSetResponder != null;
+
+const PRESS_RECT_OFFSET = { top: 20, left: 20, right: 20, bottom: 30 };
+
+class Text extends Component<*, *> {
   static displayName = 'Text';
 
   static propTypes = TextPropTypes;
@@ -29,6 +45,40 @@ class Text extends Component<*> {
     isInAParentText: bool
   };
 
+  static getDerivedStateFromProps(nextProps: *, prevState: *): * | null {
+    return prevState.responseHandlers == null && isTouchable(nextProps)
+      ? {
+          responseHandlers: prevState.createResponderHandlers()
+        }
+      : null;
+  }
+
+  constructor(props) {
+    super(props);
+    if (isTouchable(props)) this._attachTouchHandlers();
+  }
+
+  state = {
+    ...Touchable.Mixin.touchableGetInitialState(),
+    isHighlighted: false,
+    createResponderHandlers: this._createResponseHandlers.bind(this),
+    responseHandlers: null
+  };
+
+  // Flow definitions for touchable events
+  touchableGetPressRectOffset: ?() => *;
+  touchableHandleActivePressIn: ?() => void;
+  touchableHandleActivePressOut: ?() => void;
+  touchableHandleKeyEvent: ?(event: *) => void;
+  touchableHandleLongPress: ?(event: *) => void;
+  touchableHandlePress: ?(event: *) => void;
+  touchableHandleResponderGrant: ?(event: *, dispatchID: string) => void;
+  touchableHandleResponderMove: ?(event: *) => void;
+  touchableHandleResponderRelease: ?(event: *) => void;
+  touchableHandleResponderTerminate: ?(event: *) => void;
+  touchableHandleResponderTerminationRequest: ?() => boolean;
+  touchableHandleStartShouldSetResponder: ?() => boolean;
+
   getChildContext() {
     return { isInAParentText: true };
   }
@@ -37,7 +87,6 @@ class Text extends Component<*> {
     const {
       dir,
       numberOfLines,
-      onPress,
       selectable,
       style,
       /* eslint-disable */
@@ -48,6 +97,7 @@ class Text extends Component<*> {
       minimumFontScale,
       onLayout,
       onLongPress,
+      onPress,
       pressRetentionOffset,
       selectionColor,
       suppressHighlighting,
@@ -59,10 +109,16 @@ class Text extends Component<*> {
 
     const { isInAParentText } = this.context;
 
-    if (onPress) {
+    if (isTouchable(this.props)) {
       otherProps.accessible = true;
-      otherProps.onClick = this._createPressHandler(onPress);
-      otherProps.onKeyDown = this._createEnterHandler(onPress);
+      otherProps.onKeyDown = this.touchableHandleKeyEvent;
+      otherProps.onKeyUp = this.touchableHandleKeyEvent;
+      otherProps.onResponderGrant = this.touchableHandleResponderGrant;
+      otherProps.onResponderMove = this.touchableHandleResponderMove;
+      otherProps.onResponderRelease = this.touchableHandleResponderRelease;
+      otherProps.onResponderTerminate = this.touchableHandleResponderTerminate;
+      otherProps.onResponderTerminationRequest = this.touchableHandleResponderTerminationRequest;
+      otherProps.onStartShouldSetResponder = this.touchableHandleStartShouldSetResponder;
     }
 
     // allow browsers to automatically infer the language writing direction
@@ -73,27 +129,89 @@ class Text extends Component<*> {
       style,
       selectable === false && styles.notSelectable,
       numberOfLines === 1 && styles.singleLineStyle,
-      onPress && styles.pressable
+      isTouchable(this.props) && styles.pressable
     ];
 
     const component = isInAParentText ? 'span' : 'div';
-
     return createElement(component, otherProps);
   }
 
-  _createEnterHandler(fn) {
-    return e => {
-      if (e.keyCode === 13) {
-        fn && fn(e);
+  _createResponseHandlers(): ResponseHandlers {
+    return {
+      onStartShouldSetResponder: (): boolean => {
+        return isTouchable(this.props);
+      },
+      onResponderGrant: (event: *, dispatchID: string): void => {
+        nullthrows(this.touchableHandleResponderGrant)(event, dispatchID);
+        if (this.props.onResponderGrant != null) {
+          this.props.onResponderGrant.call(this, event, dispatchID);
+        }
+      },
+      onResponderMove: (event: *): void => {
+        nullthrows(this.touchableHandleResponderMove)(event);
+        if (this.props.onResponderMove != null) {
+          this.props.onResponderMove.call(this, event);
+        }
+      },
+      onResponderRelease: (event: *): void => {
+        nullthrows(this.touchableHandleResponderRelease)(event);
+        if (this.props.onResponderRelease != null) {
+          this.props.onResponderRelease.call(this, event);
+        }
+      },
+      onResponderTerminate: (event: *): void => {
+        nullthrows(this.touchableHandleResponderTerminate)(event);
+        if (this.props.onResponderTerminate != null) {
+          this.props.onResponderTerminate.call(this, event);
+        }
+      },
+      onResponderTerminationRequest: (): boolean => {
+        const { onResponderTerminationRequest } = this.props;
+        if (!nullthrows(this.touchableHandleResponderTerminationRequest)()) {
+          return false;
+        }
+        if (onResponderTerminationRequest == null) {
+          return true;
+        }
+        return onResponderTerminationRequest();
       }
     };
   }
 
-  _createPressHandler(fn) {
-    return e => {
-      e.stopPropagation();
-      fn && fn(e);
+  /**
+   * Lazily attaches Touchable.Mixin handlers.
+   */
+  _attachTouchHandlers(): void {
+    if (this.touchableGetPressRectOffset != null) {
+      return;
+    }
+    for (const key in Touchable.Mixin) {
+      if (typeof Touchable.Mixin[key] === 'function') {
+        (this: any)[key] = Touchable.Mixin[key].bind(this);
+      }
+    }
+    this.touchableHandleActivePressIn = (): void => {
+      if (!this.props.suppressHighlighting && isTouchable(this.props)) {
+        this.setState({ isHighlighted: true });
+      }
     };
+    this.touchableHandleActivePressOut = (): void => {
+      if (!this.props.suppressHighlighting && isTouchable(this.props)) {
+        this.setState({ isHighlighted: false });
+      }
+    };
+    this.touchableHandlePress = (event: *): void => {
+      if (this.props.onPress != null) {
+        this.props.onPress(event);
+      }
+    };
+    this.touchableHandleLongPress = (event: *): void => {
+      if (this.props.onLongPress != null) {
+        this.props.onLongPress(event);
+      }
+    };
+    this.touchableGetPressRectOffset = (): * =>
+      this.props.pressRetentionOffset == null ? PRESS_RECT_OFFSET : this.props.pressRetentionOffset;
   }
 }
 
