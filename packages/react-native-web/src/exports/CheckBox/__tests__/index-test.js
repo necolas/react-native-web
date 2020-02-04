4f8d2918b7d7f4ebb6bccb0bfd556b3b0b124ff5
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-02-04 14:38:19 -0800

    [change] modernize CheckBox
    Rewrite CheckBox to use function components and hooks.
    Rewrite the tests to replace enzyme with testing-library.

--- packages/react-native-web/src/exports/CheckBox/__tests__/index-test.js
+++ packages/react-native-web/src/exports/CheckBox/__tests__/index-test.js
@@ -2,59 +2,62 @@
 
 import CheckBox from '../';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 
-const checkboxSelector = 'input[type="checkbox"]';
+function findCheckbox(container) {
+  return container.firstChild.querySelector('input');
+}
 
 describe('CheckBox', () => {
   describe('disabled', () => {
     test('when "false" a default checkbox is rendered', () => {
-      const component = shallow(<CheckBox />);
-      expect(component.find(checkboxSelector).prop('disabled')).toBe(undefined);
+      const { container } = render(<CheckBox />);
+      expect(findCheckbox(container).disabled).toBe(false);
     });
 
     test('when "true" a disabled checkbox is rendered', () => {
-      const component = shallow(<CheckBox disabled />);
-      expect(component.find(checkboxSelector).prop('disabled')).toBe(true);
+      const { container } = render(<CheckBox disabled />);
+      expect(findCheckbox(container).disabled).toBe(true);
     });
   });
 
   describe('onChange', () => {
     test('is called with the event object', () => {
       const onChange = jest.fn();
-      const component = shallow(<CheckBox onChange={onChange} value={false} />);
-      component.find('input').simulate('change', { nativeEvent: { target: { checked: true } } });
-      expect(onChange).toHaveBeenCalledWith({
-        nativeEvent: { target: { checked: true }, value: true }
-      });
+      const { container } = render(<CheckBox onChange={onChange} value={false} />);
+      const checkbox = findCheckbox(container);
+      checkbox.click(); // Needed to get ReactDOM to trigger 'change' event
+      expect(onChange).toHaveBeenCalled();
     });
   });
 
   describe('onValueChange', () => {
     test('when value is "false" it receives "true"', () => {
       const onValueChange = jest.fn();
-      const component = shallow(<CheckBox onValueChange={onValueChange} value={false} />);
-      component.find('input').simulate('change', { nativeEvent: { target: { checked: true } } });
+      const { container } = render(<CheckBox onValueChange={onValueChange} value={false} />);
+      const checkbox = findCheckbox(container);
+      checkbox.click(); // Needed to get ReactDOM to trigger 'change' event
       expect(onValueChange).toHaveBeenCalledWith(true);
     });
 
     test('when value is "true" it receives "false"', () => {
       const onValueChange = jest.fn();
-      const component = shallow(<CheckBox onValueChange={onValueChange} value />);
-      component.find('input').simulate('change', { nativeEvent: { target: { checked: false } } });
+      const { container } = render(<CheckBox onValueChange={onValueChange} value />);
+      const checkbox = findCheckbox(container);
+      checkbox.click(); // Needed to get ReactDOM to trigger 'change' event
       expect(onValueChange).toHaveBeenCalledWith(false);
     });
   });
 
   describe('value', () => {
     test('when "false" an unchecked checkbox is rendered', () => {
-      const component = shallow(<CheckBox value={false} />);
-      expect(component.find(checkboxSelector).prop('checked')).toBe(false);
+      const { container } = render(<CheckBox value={false} />);
+      expect(findCheckbox(container).checked).toBe(false);
     });
 
     test('when "true" a checked checkbox is rendered', () => {
-      const component = shallow(<CheckBox value />);
-      expect(component.find(checkboxSelector).prop('checked')).toBe(true);
+      const { container } = render(<CheckBox value />);
+      expect(findCheckbox(container).checked).toBe(true);
     });
   });
 });
