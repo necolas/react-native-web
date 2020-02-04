756a861f7dff251d37ab8144ee94adc4dc46d458
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-02-04 11:30:57 -0800

    [change] modernize ProgressBar
    Rewrite ProgressBar to use function components and hooks.
    Rewrite the tests to replace enzyme with testing-library.

--- packages/react-native-web/src/exports/ProgressBar/__tests__/index-test.js
+++ packages/react-native-web/src/exports/ProgressBar/__tests__/index-test.js
@@ -1,19 +1,19 @@
 /* eslint-env jasmine, jest */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import ProgressBar from '..';
 
 describe('components/ProgressBar', () => {
   describe('progress', () => {
-    it('value as percentage is set to "aria-valuenow"', () => {
-      const component = shallow(<ProgressBar progress={0.5} />);
-      expect(component.prop('aria-valuenow') === 50).toBeTruthy();
+    test('value as percentage is set to "aria-valuenow"', () => {
+      const { container } = render(<ProgressBar progress={0.5} />);
+      expect(container.firstChild.getAttribute('aria-valuenow')).toBe('50');
     });
 
-    it('is ignored when "indeterminate" is "true"', () => {
-      const component = shallow(<ProgressBar indeterminate progress={0.5} />);
-      expect(component.prop('aria-valuenow') === null).toBeTruthy();
+    test('is ignored when "indeterminate" is "true"', () => {
+      const { container } = render(<ProgressBar indeterminate progress={0.5} />);
+      expect(container.firstChild.getAttribute('aria-valuenow')).toBe(null);
     });
   });
 });
