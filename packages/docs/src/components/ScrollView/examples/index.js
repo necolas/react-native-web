c61a95f39afb87dd5de61d93b3950c9a14555ecf
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-02-13 14:46:47 -0800

    Fix props table for docs
    Stop using the built-in Props table from Storybook (which wasn't working.)

---
+++ packages/docs/src/components/ScrollView/examples/index.js
@@ -0,0 +1,4 @@
+export { default as horizontal } from './Horizontal';
+export { default as onScroll } from './OnScroll';
+export { default as scrollEnabled } from './ScrollEnabled';
+export { default as vertical } from './Vertical';
---
+++ packages/docs/src/components/TouchableWithoutFeedback/examples/index.js
@@ -0,0 +1,3 @@
+export { default as delayEvents } from './DelayEvents';
+export { default as disabled } from './Disabled';
+export { default as feedbackEvents } from './FeedbackEvents';
