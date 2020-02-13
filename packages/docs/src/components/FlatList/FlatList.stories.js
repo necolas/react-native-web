52b9ec0da1f9ea279bbda5c86e714caa594f4673
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-02-13 14:46:47 -0800

    Fix props table for docs
    Stop using the built-in Props table from Storybook (which wasn't working.)

--- packages/docs/src/components/FlatList/FlatList.stories.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: 'Components|FlatList',
-  includeStories: []
-};
-
-export { default as singleColumn } from './examples/SingleColumn';
-export { default as multiColumn } from './examples/MultiColumn';
