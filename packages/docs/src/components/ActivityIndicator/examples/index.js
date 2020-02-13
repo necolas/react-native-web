9b2d0234116ef6ea85e403debe2d8808ddfd4876
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-02-13 14:46:47 -0800

    Fix props table for docs
    Stop using the built-in Props table from Storybook (which wasn't working.)

---
+++ packages/docs/src/components/ActivityIndicator/examples/index.js
@@ -0,0 +1,4 @@
+export { default as animating } from './Animating';
+export { default as color } from './Color';
+export { default as hidesWhenStopped } from './HidesWhenStopped';
+export { default as size } from './Size';
