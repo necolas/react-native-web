42ad6d7ec7be7409305097bee58617d510ae9d68
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-03-31 14:06:01 -0700

    [change] Text and View onClick handling
    Makes `onClick` part of the stable props API. In the future this will be used
    to implement `onPress` in the Touchables/Pressables. Special handling of click
    for keyboards is performed in `createElement`. At the moment, `Text` still
    includes the `onPress` prop, which will only be called if `onClick` is not also
    being used. In the future `Text` (in React Native) should remove the Touchable
    props from its API.

--- packages/react-native-web/src/exports/createElement/index.js
+++ packages/react-native-web/src/exports/createElement/index.js
@@ -11,9 +11,15 @@ import AccessibilityUtil from '../../modules/AccessibilityUtil';
 import createDOMProps from '../../modules/createDOMProps';
 import React from 'react';
 
-const adjustProps = domProps => {
+const createElement = (component, props, ...children) => {
+  // Use equivalent platform elements where possible.
+  let accessibilityComponent;
+  if (component && component.constructor === String) {
+    accessibilityComponent = AccessibilityUtil.propsToAccessibilityComponent(props);
+  }
+  const Component = accessibilityComponent || component;
+  const domProps = createDOMProps(Component, props);
   const { onClick, role } = domProps;
-
   const isButtonLikeRole = AccessibilityUtil.buttonLikeRoles[role];
   const isDisabled = AccessibilityUtil.isDisabled(domProps);
 
@@ -24,26 +30,16 @@ const adjustProps = domProps => {
 
   // Button-like roles should trigger 'onClick' if SPACE or ENTER keys are pressed.
   if (isButtonLikeRole && !isDisabled) {
-    domProps.onKeyPress = function(e) {
-      if (!e.isDefaultPrevented() && (e.which === 13 || e.which === 32)) {
+    domProps.onKeyDown = function(e) {
+      if (!e.isDefaultPrevented() && (e.key === 'Enter' || e.key === ' ')) {
         e.preventDefault();
-        if (onClick) {
+        if (onClick != null) {
           onClick(e);
         }
       }
     };
   }
-};
 
-const createElement = (component, props, ...children) => {
-  // use equivalent platform elements where possible
-  let accessibilityComponent;
-  if (component && component.constructor === String) {
-    accessibilityComponent = AccessibilityUtil.propsToAccessibilityComponent(props);
-  }
-  const Component = accessibilityComponent || component;
-  const domProps = createDOMProps(Component, props);
-  adjustProps(domProps);
   return React.createElement(Component, domProps, ...children);
 };
 
