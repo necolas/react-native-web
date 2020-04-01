5870d3baffd64cc9a62406e2b6d1fbed2d32e282
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-03-31 17:25:17 -0700

    Remove enzyme

--- scripts/jest/setupFramework.js
+++ scripts/jest/setupFramework.js
@@ -1,10 +1,1 @@
 /* eslint-env jasmine, jest */
-
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme from 'enzyme';
-import serializer from './serializer';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-// TODO: move off legacy serializer
-expect.addSnapshotSerializer(serializer);
