37ff6b48885bb38079db7e8b4739078dc360be19
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-04-20 11:40:05 -0700

    Reorganize usePressEvents and PressResponder

--- packages/react-native-web/src/exports/TouchableHighlight/index.js
+++ packages/react-native-web/src/exports/TouchableHighlight/index.js
@@ -16,7 +16,7 @@ import type { ViewProps } from '../View';
 
 import * as React from 'react';
 import { useCallback, useMemo, useState, useRef } from 'react';
-import usePressEvents from '../../modules/PressResponder/usePressEvents';
+import usePressEvents from '../../hooks/usePressEvents';
 import setAndForwardRef from '../../modules/setAndForwardRef';
 import StyleSheet from '../StyleSheet';
 import View from '../View';
