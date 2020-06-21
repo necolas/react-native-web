79c05431dc6946e93be58dc552ecc7b95422408d
James Ward <james@notjam.es>
2020-06-21 18:06:18 -0400

    clean up comments to add project license

--- packages/react-native-web/src/exports/Modal/FocusBracket.js
+++ packages/react-native-web/src/exports/Modal/FocusBracket.js
@@ -1,12 +1,25 @@
+/**
+ * Copyright (c) Nicolas Gallagher.
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
 import React from 'react';
 
 import Text from '../Text';
 import StyleSheet from '../StyleSheet';
 
-// This is used to wrap "wrap" the dialog we're opening so that
-// when you're tab focusing you'll never leave the document and
-// we still trap the focus even if the dialog is at the start
-// or end of the document.
+/**
+ * This Component is used to "wrap" the modal we're opening
+ * so that changing focus via tab will never leave the document.
+ *
+ * This allowsus to properly trap the focus within a modal
+ * even if the modal is at the start or end of a document.
+ */
 export default class FocusBracket extends React.PureComponent<> {
   ref = React.createRef();
 
