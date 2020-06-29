728e20ff1fa24b0976dccfa65a3547043a72882f
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-06-29 09:59:12 -0700

    [fix] jest preset
    Fix #1654

--- packages/react-native-web/jest/setup.js
+++ packages/react-native-web/jest/setup.js
@@ -7,12 +7,12 @@ global.__DEV__ = true;
 const mockEmptyObject = {};
 
 // Make sure snapshots contain the original style objects
-jest.mock('../dist/cjs/modules/StyleSheet/ReactNativePropRegistry', () => ({
+jest.mock('../dist/cjs/exports/StyleSheet/ReactNativePropRegistry', () => ({
   register: id => id,
   getByID: () => mockEmptyObject
 }));
 
-jest.mock('../dist/modules/StyleSheet/ReactNativePropRegistry', () => ({
+jest.mock('../dist/exports/StyleSheet/ReactNativePropRegistry', () => ({
   register: id => id,
   getByID: () => mockEmptyObject
 }));
