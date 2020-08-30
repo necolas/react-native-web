c5ebf2d3594eaeff39aceeae109a9189bdf57938
James Ward <james@notjam.es>
2020-08-30 17:57:59 -0400

    fix example exports

--- packages/docs/src/components/Modal/examples/index.js
+++ packages/docs/src/components/Modal/examples/index.js
@@ -1,4 +1,4 @@
-export { default as transparentModal } from './examples/Transparent';
-export { default as simpleModal } from './examples/Simple';
-export { default as animatedModal } from './examples/Animated';
-export { default as modalception } from './examples/Modalception';
+export { default as transparentModal } from './Transparent';
+export { default as simpleModal } from './Simple';
+export { default as animatedModal } from './Animated';
+export { default as modalception } from './Modalception';
