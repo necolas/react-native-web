40549e191c2563a8b221138e5d2ba66742a09dae
James Ward <james@notjam.es>
2020-08-30 17:48:26 -0400

    clean up Modal docs

---
+++ packages/docs/src/components/Modal/examples/index.js
@@ -0,0 +1,4 @@
+export { default as transparentModal } from './examples/Transparent';
+export { default as simpleModal } from './examples/Simple';
+export { default as animatedModal } from './examples/Animated';
+export { default as modalception } from './examples/Modalception';
