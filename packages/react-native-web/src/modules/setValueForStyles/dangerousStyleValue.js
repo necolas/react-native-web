24c027e772f40677183248c61a000f981c165dcd
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-08-31 16:58:35 -0700

    Remove react-dom vendor directory

--- packages/react-native-web/src/modules/setValueForStyles/dangerousStyleValue.js
+++ packages/react-native-web/src/modules/setValueForStyles/dangerousStyleValue.js
@@ -10,7 +10,7 @@
  * @noflow
  */
 
-import isUnitlessNumber from '../../../modules/unitlessNumbers';
+import isUnitlessNumber from '../unitlessNumbers';
 
 /**
  * Convert a value into the proper css writable value. The style name `name`
--- packages/react-native-web/src/modules/setValueForStyles/index.js
+++ packages/react-native-web/src/modules/setValueForStyles/index.js
@@ -10,7 +10,7 @@
  * @noflow
  */
 
-import dangerousStyleValue from '../dangerousStyleValue';
+import dangerousStyleValue from './dangerousStyleValue';
 
 /**
  * Sets the value for multiple styles on a node.  If a value is specified as
