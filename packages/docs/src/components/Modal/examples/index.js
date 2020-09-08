d97a1ca5671aa91cab6d0825677a5e35ce3c6685
James Ward <james@notjam.es>
2020-09-08 11:45:37 -0700

    [add] Modal component
    This adds support for the React Native Modal on web.
    
    The app content is hidden from screen readers by setting the aria-modal flag on
    the modal. This focus is trapped within the modal, both when attempting to
    focus elsewhere using the mouse as well as when attempting to focus elsewhere
    using the keyboard. A built-in "Escape to close" mechanism is been implemented
    that calls 'onRequestClose' for the active modal.
    
    Close #1646
    Fix #1020

---
+++ packages/docs/src/components/Modal/examples/index.js
@@ -0,0 +1,4 @@
+export { default as transparentModal } from './Transparent';
+export { default as simpleModal } from './Simple';
+export { default as animatedModal } from './Animated';
+export { default as modalception } from './Modalception';
