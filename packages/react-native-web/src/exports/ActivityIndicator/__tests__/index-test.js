0fb3036f318d0ed51a970f7d13e13080ad891c83
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-10-01 16:40:51 -0700

    Add to ActivityIndicator tests

--- packages/react-native-web/src/exports/ActivityIndicator/__tests__/index-test.js
+++ packages/react-native-web/src/exports/ActivityIndicator/__tests__/index-test.js
@@ -2,9 +2,25 @@
 
 import ActivityIndicator from '..';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { createEventTarget } from 'dom-event-testing-library';
 import { render } from '@testing-library/react';
 
 describe('components/ActivityIndicator', () => {
+  describe('prop "accessibilityLabel"', () => {
+    test('value is set', () => {
+      const { container } = render(<ActivityIndicator accessibilityLabel="accessibility label" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "accessibilityLiveRegion"', () => {
+    test('value is set', () => {
+      const { container } = render(<ActivityIndicator accessibilityLiveRegion="polite" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
   describe('prop "animating"', () => {
     test('is "true"', () => {
       const { container } = render(<ActivityIndicator animating={true} />);
@@ -23,6 +39,13 @@ describe('components/ActivityIndicator', () => {
     expect(svg).toMatchSnapshot();
   });
 
+  describe('prop "dataSet"', () => {
+    test('value is set', () => {
+      const { container } = render(<ActivityIndicator dataSet={{ one: 'one', two: 'two' }} />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
   describe('prop "hidesWhenStopped"', () => {
     test('is "true"', () => {
       const { container } = render(<ActivityIndicator animating={false} hidesWhenStopped={true} />);
@@ -37,6 +60,52 @@ describe('components/ActivityIndicator', () => {
     });
   });
 
+  describe('prop "nativeID"', () => {
+    test('value is set', () => {
+      const { container } = render(<ActivityIndicator nativeID="123" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "onBlur"', () => {
+    test('is called', () => {
+      const onBlur = jest.fn();
+      const ref = React.createRef();
+      act(() => {
+        render(<ActivityIndicator onBlur={onBlur} ref={ref} />);
+      });
+      const target = createEventTarget(ref.current);
+      act(() => {
+        target.focus();
+        target.blur();
+      });
+      expect(onBlur).toBeCalled();
+    });
+  });
+
+  describe('prop "onFocus"', () => {
+    test('is called', () => {
+      const onFocus = jest.fn();
+      const ref = React.createRef();
+      act(() => {
+        render(<ActivityIndicator onFocus={onFocus} ref={ref} />);
+      });
+      const target = createEventTarget(ref.current);
+      act(() => {
+        target.focus();
+      });
+      expect(onFocus).toBeCalled();
+    });
+  });
+
+  describe('prop "ref"', () => {
+    test('value is set', () => {
+      const ref = jest.fn();
+      render(<ActivityIndicator ref={ref} />);
+      expect(ref).toBeCalled();
+    });
+  });
+
   describe('prop "size"', () => {
     test('is "large"', () => {
       const { container } = render(<ActivityIndicator size="large" />);
@@ -45,6 +114,20 @@ describe('components/ActivityIndicator', () => {
 
     test('is a number', () => {
       const { container } = render(<ActivityIndicator size={30} />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "style"', () => {
+    test('value is set', () => {
+      const { container } = render(<ActivityIndicator style={{ borderWidth: 5 }} />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "testID"', () => {
+    test('value is set', () => {
+      const { container } = render(<ActivityIndicator testID="123" />);
       expect(container.firstChild).toMatchSnapshot();
     });
   });
