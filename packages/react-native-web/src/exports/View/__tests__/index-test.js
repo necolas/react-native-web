72bfe499c536db08984853e011fc36bf8a4cf85b
Nicolas Gallagher <nicolasgallagher@gmail.com>
2020-10-05 14:28:27 -0700

    Add to View tests

--- packages/react-native-web/src/exports/View/__tests__/index-test.js
+++ packages/react-native-web/src/exports/View/__tests__/index-test.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import View from '../';
+import { act } from 'react-dom/test-utils';
+import { createEventTarget } from 'dom-event-testing-library';
 import { render } from '@testing-library/react';
 
 describe('components/View', () => {
@@ -43,8 +45,105 @@ describe('components/View', () => {
     });
   });
 
+  describe('prop "accessibilityLabel"', () => {
+    test('value is set', () => {
+      const { container } = render(<View accessibilityLabel="accessibility label" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "accessibilityLiveRegion"', () => {
+    test('value is set', () => {
+      const { container } = render(<View accessibilityLiveRegion="polite" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "accessibilityRole"', () => {
+    test('value is set', () => {
+      const { container } = render(<View accessibilityRole="none" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+
+    test('value is "button"', () => {
+      const { container } = render(<View accessibilityRole="button" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+
+    test('value alters HTML element', () => {
+      const { container } = render(<View accessibilityRole="link" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  test('allows "dir" to be overridden', () => {
+    const { container } = render(<View dir="rtl" />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  describe('prop "nativeID"', () => {
+    test('value is set', () => {
+      const { container } = render(<View nativeID="nativeID" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "onBlur"', () => {
+    test('is called', () => {
+      const onBlur = jest.fn();
+      const ref = React.createRef();
+      act(() => {
+        render(<View onBlur={onBlur} ref={ref} />);
+      });
+      const target = createEventTarget(ref.current);
+      act(() => {
+        target.focus();
+        target.blur();
+      });
+      expect(onBlur).toBeCalled();
+    });
+  });
+
+  describe('prop "onFocus"', () => {
+    test('is called', () => {
+      const onFocus = jest.fn();
+      const ref = React.createRef();
+      act(() => {
+        render(<View onFocus={onFocus} ref={ref} />);
+      });
+      const target = createEventTarget(ref.current);
+      act(() => {
+        target.focus();
+        target.blur();
+      });
+      expect(onFocus).toBeCalled();
+    });
+  });
+
+  describe('prop "ref"', () => {
+    test('value is set', () => {
+      const ref = jest.fn();
+      render(<View ref={ref} />);
+      expect(ref).toBeCalled();
+    });
+  });
+
   test('prop "pointerEvents"', () => {
     const { container } = render(<View pointerEvents="box-only" />);
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  describe('prop "style"', () => {
+    test('value is set', () => {
+      const { container } = render(<View style={{ borderWidth: 5 }} />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('prop "testID"', () => {
+    test('value is set', () => {
+      const { container } = render(<View testID="123" />);
+      expect(container.firstChild).toMatchSnapshot();
+    });
   });
 });
