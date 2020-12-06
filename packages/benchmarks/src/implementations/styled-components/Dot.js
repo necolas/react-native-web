bb0f92e903ff0937ad12b6fea29d9fb470232aba
Evgeniy Dzhumak <elforastero@ya.ru>
2020-12-06 15:35:26 +0400

    [fix]: styled-components dynamic benchmark

--- packages/benchmarks/src/implementations/styled-components/Dot.js
+++ packages/benchmarks/src/implementations/styled-components/Dot.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 import View from './View';
 
-const Dot = styled(View).attrs({
-  style: props => ({
+const Dot = styled(View).attrs(props => ({
+  style: {
     marginLeft: `${props.x}px`,
     marginTop: `${props.y}px`,
     borderRightWidth: `${props.size / 2}px`,
     borderBottomWidth: `${props.size / 2}px`,
     borderLeftWidth: `${props.size / 2}px`,
     borderBottomColor: `${props.color}`
-  })
-})`
+  }
+}))`
   position: absolute;
   cursor: pointer;
   width: 0;
