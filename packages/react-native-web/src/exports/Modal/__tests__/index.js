2ef31c483e80c17602480d487511819725680c00
Rafał Zakrzewski <zakrzewski.rafal@gmail.com>
2021-01-19 11:59:20 +0100

    tests: implement test for refocusing mechanism after closing modal

--- packages/react-native-web/src/exports/Modal/__tests__/index.js
+++ packages/react-native-web/src/exports/Modal/__tests__/index.js
@@ -280,6 +280,121 @@ describe('components/Modal', () => {
     expect(document.activeElement).toBe(insideElement);
   });
 
+  test('focus is brought back to the element that triggered modal after closing', () => {
+    const { rerender } = render(
+      <>
+        <a data-testid={'outside'} href={'#outside'}>
+          Outside
+        </a>
+        <Modal visible={false}>
+          <a data-testid={'inside'} href={'#hello'}>
+            Hello
+          </a>
+        </Modal>
+        <a data-testid={'modal-trigger'} href={'#modal-trigger'}>
+          Outside
+        </a>
+      </>
+    );
+
+    const modalTrigger = document.querySelector('[data-testid="modal-trigger"]');
+    modalTrigger.focus();
+    expect(document.activeElement).toBe(modalTrigger);
+
+    rerender(
+      <>
+        <a data-testid={'outside'} href={'#outside'}>
+          Outside
+        </a>
+        <Modal visible={true}>
+          <a data-testid={'inside'} href={'#hello'}>
+            Hello
+          </a>
+        </Modal>
+        <a data-testid={'modal-trigger'} href={'#modal-trigger'}>
+          Outside
+        </a>
+      </>
+    );
+
+    const insideElement = document.querySelector('[data-testid="inside"]');
+    expect(document.activeElement).toBe(insideElement);
+
+    rerender(
+      <>
+        <a data-testid={'outside'} href={'#outside'}>
+          Outside
+        </a>
+        <Modal visible={false}>
+          <a data-testid={'inside'} href={'#hello'}>
+            Hello
+          </a>
+        </Modal>
+        <a data-testid={'modal-trigger'} href={'#modal-trigger'}>
+          Outside
+        </a>
+      </>
+    );
+
+    expect(document.activeElement).toBe(modalTrigger);
+  });
+
+  test('focus is brought back to the body when element that triggered modal is removed from the DOM after closing modal', () => {
+    const { rerender } = render(
+      <>
+        <a data-testid={'outside'} href={'#outside'}>
+          Outside
+        </a>
+        <Modal visible={false}>
+          <a data-testid={'inside'} href={'#hello'}>
+            Hello
+          </a>
+        </Modal>
+        <a data-testid={'modal-trigger'} href={'#modal-trigger'}>
+          Outside
+        </a>
+      </>
+    );
+
+    const modalTrigger = document.querySelector('[data-testid="modal-trigger"]');
+    modalTrigger.focus();
+    expect(document.activeElement).toBe(modalTrigger);
+
+    rerender(
+      <>
+        <a data-testid={'outside'} href={'#outside'}>
+          Outside
+        </a>
+        <Modal visible={true}>
+          <a data-testid={'inside'} href={'#hello'}>
+            Hello
+          </a>
+        </Modal>
+        <a data-testid={'modal-trigger'} href={'#modal-trigger'}>
+          Outside
+        </a>
+      </>
+    );
+
+    const insideElement = document.querySelector('[data-testid="inside"]');
+    expect(document.activeElement).toBe(insideElement);
+
+    rerender(
+      <>
+        <a data-testid={'outside'} href={'#outside'}>
+          Outside
+        </a>
+        <Modal visible={false}>
+          <a data-testid={'inside'} href={'#hello'}>
+            Hello
+          </a>
+        </Modal>
+      </>
+    );
+
+    expect(document.activeElement).toBe(document.body);
+  });
+
   test('focus is trapped when active', () => {
     render(
       <>
@@ -418,10 +533,14 @@ describe('components/Modal', () => {
 
     function TestComponent() {
       React.useEffect(() => spy('mount'), []);
-      return <Modal visible={true}><a ref={(ref) => ref ? spy('ref') : spy('noref')} /></Modal>;
+      return (
+        <Modal visible={true}>
+          <a ref={ref => (ref ? spy('ref') : spy('noref'))} />
+        </Modal>
+      );
     }
 
-    render(<TestComponent />)
+    render(<TestComponent />);
 
     expect(spy).toHaveBeenNthCalledWith(1, 'ref');
     expect(spy).toHaveBeenNthCalledWith(2, 'mount');
