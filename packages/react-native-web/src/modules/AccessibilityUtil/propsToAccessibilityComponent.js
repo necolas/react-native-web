d6d28a48863d1e435808443a2b394c53535c1c64
Nicolas Gallagher <nicolasgallagher@gmail.com>
2021-02-05 15:54:56 -0800

    [add] More role to component mappings

--- packages/react-native-web/src/modules/AccessibilityUtil/propsToAccessibilityComponent.js
+++ packages/react-native-web/src/modules/AccessibilityUtil/propsToAccessibilityComponent.js
@@ -12,15 +12,22 @@ import propsToAriaRole from './propsToAriaRole';
 const roleComponents = {
   article: 'article',
   banner: 'header',
+  blockquote: 'blockquote',
+  code: 'code',
   complementary: 'aside',
   contentinfo: 'footer',
+  deletion: 'del',
+  emphasis: 'em',
+  figure: 'figure',
+  insertion: 'ins',
   form: 'form',
   link: 'a',
   list: 'ul',
   listitem: 'li',
   main: 'main',
   navigation: 'nav',
-  region: 'section'
+  region: 'section',
+  strong: 'strong'
 };
 
 const emptyObject = {};
