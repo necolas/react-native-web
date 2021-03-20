56dd0fb656fd6c166e59d31c65a3d2b85e532762
Charlie Croom <ccroom@twitter.com>
2021-03-20 10:40:43 -0400

    Move flow-typed files into default directory
    Close #1953

--- types/create-react-class.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: 121e589c9be7dac408c14a02e88287fa
-// flow-typed version: a1a20d4928/create-react-class_v15.x.x/flow_>=v0.41.x
-
-declare module 'create-react-class' {
-  declare var exports: React$CreateClass;
-}
