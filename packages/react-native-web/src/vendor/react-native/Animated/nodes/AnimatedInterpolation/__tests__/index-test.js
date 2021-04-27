ca67e5b5d792a3fd9bf7d5176448a93fed250c4a
Andreas Kjellqvist <linco95@gmail.com>
2021-04-27 19:25:32 +0200

    Parameterized test to add number test as well

--- packages/react-native-web/src/vendor/react-native/Animated/nodes/AnimatedInterpolation/__tests__/index-test.js
+++ packages/react-native-web/src/vendor/react-native/Animated/nodes/AnimatedInterpolation/__tests__/index-test.js
@@ -5,18 +5,18 @@ import AnimatedValue from '../../AnimatedValue';
 
 describe('AnimatedInterpolation', () => {
   describe('interpolate', () => {
-    test('can interpolate hex strings', () => {
-      const config = {
-        inputRange: [0, 1],
-        outputRange: [
-          "#1E1E1E",
-          "#1E1E1F",
-        ]
-      };
-      const animVal = new AnimatedValue(0)
-      const animInterpol = new AnimatedInterpolation(animVal, config);
-      const result = animInterpol.interpolate(config)
-      expect(result).toBeInstanceOf(AnimatedInterpolation)
-    })
+    test.each([
+      [["#1E1E1E", "#1E1E1F"]],
+      [[0x1E1E1E, 0x1E1E1F]]
+    ])('can interpolate %s: %s', (outputRange) => {
+        const config = {
+          inputRange: [0, 1],
+          outputRange
+        };
+        const animVal = new AnimatedValue(0)
+        const animInterpol = new AnimatedInterpolation(animVal, config);
+        const result = animInterpol.interpolate(config)
+        expect(result).toBeInstanceOf(AnimatedInterpolation)
+      })
   })
 });
