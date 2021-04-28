a2a535ac28f886968eb69da059e158e4c11181cc
Andreas Kjellqvist <linco95@gmail.com>
2021-04-28 15:21:08 +0200

    changed to normalize-css-color instead of modules/normalizeColor in AnimatedInterpolation

--- packages/react-native-web/src/vendor/react-native/Animated/nodes/AnimatedInterpolation.js
+++ packages/react-native-web/src/vendor/react-native/Animated/nodes/AnimatedInterpolation.js
@@ -17,7 +17,7 @@ import AnimatedWithChildren from './AnimatedWithChildren';
 import NativeAnimatedHelper from '../NativeAnimatedHelper';
 
 import invariant from 'fbjs/lib/invariant';
-import normalizeColor from '../../../../modules/normalizeColor';
+import normalizeColor from 'normalize-css-color';
 
 const __DEV__ = process.env.NODE_ENV !== 'production';
 
