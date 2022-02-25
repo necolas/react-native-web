e7a99f5ef2aebd213fa8a03b19fa47f701b4ab97
Filip Holm <filip@frapper.se>
2022-02-25 10:38:10 +0100

    [change] Show focus ring on CheckBox and Switch
    Close #2242

--- packages/react-native-web/src/exports/CheckBox/index.js
+++ packages/react-native-web/src/exports/CheckBox/index.js
@@ -116,7 +116,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     height: '100%',
     margin: 0,
-    opacity: 0,
+    appearance: 'none',
     padding: 0,
     width: '100%'
   }
--- packages/react-native-web/src/exports/Switch/index.js
+++ packages/react-native-web/src/exports/Switch/index.js
@@ -219,7 +219,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     height: '100%',
     margin: 0,
-    opacity: 0,
+    appearance: 'none',
     padding: 0,
     width: '100%'
   }
