360ae3b9235d10fa84282d8cd4dd07d1641a13ca
LucioChavezFuentes <luciobertinchavez@gmail.com>
2022-10-24 20:49:04 -0500

    remove unneccesary changes to onLayout

--- packages/react-native-web/src/exports/UIManager/index.js
+++ packages/react-native-web/src/exports/UIManager/index.js
@@ -17,8 +17,7 @@ const getRect = (node) => {
   const { x, y, top, left } = getBoundingClientRect(node);
   const width = node.offsetWidth;
   const height = node.offsetHeight;
-  const offsetTop = node.offsetTop;
-  return { x, y, width, height, top, left, offsetTop };
+  return { x, y, width, height, top, left };
 };
 
 const measureLayout = (node, relativeToNativeNode, callback) => {
@@ -26,10 +25,10 @@ const measureLayout = (node, relativeToNativeNode, callback) => {
   if (node && relativeNode) {
     setTimeout(() => {
       const relativeRect = getBoundingClientRect(relativeNode);
-      const { height, left, top, width, offsetTop } = getRect(node);
+      const { height, left, top, width } = getRect(node);
       const x = left - relativeRect.left;
       const y = top - relativeRect.top;
-      callback(x, y, width, height, left, top, offsetTop);
+      callback(x, y, width, height, left, top);
     }, 0);
   }
 };
--- packages/react-native-web/src/modules/useElementLayout/index.js
+++ packages/react-native-web/src/modules/useElementLayout/index.js
@@ -29,23 +29,20 @@ function getResizeObserver(): ?ResizeObserver {
           if (typeof onLayout === 'function') {
             // We still need to measure the view because browsers don't yet provide
             // border-box dimensions in the entry
-            UIManager.measure(
-              node,
-              (x, y, width, height, left, top, offsetTop) => {
-                const event: LayoutEvent = {
-                  // $FlowFixMe
-                  nativeEvent: {
-                    layout: { x, y, width, height, left, top, offsetTop }
-                  },
-                  timeStamp: Date.now()
-                };
-                Object.defineProperty(event.nativeEvent, 'target', {
-                  enumerable: true,
-                  get: () => entry.target
-                });
-                onLayout(event);
-              }
-            );
+            UIManager.measure(node, (x, y, width, height, left, top) => {
+              const event: LayoutEvent = {
+                // $FlowFixMe
+                nativeEvent: {
+                  layout: { x, y, width, height, left, top }
+                },
+                timeStamp: Date.now()
+              };
+              Object.defineProperty(event.nativeEvent, 'target', {
+                enumerable: true,
+                get: () => entry.target
+              });
+              onLayout(event);
+            });
           }
         });
       });
