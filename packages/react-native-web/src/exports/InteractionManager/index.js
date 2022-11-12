3db9b387a329ea755ebd069e54f028b20b237189
ntdiary <2471314@gmail.com>
2022-11-12 15:57:56 +0800

    Add task queue for InteractionManager
    Close #2399

--- packages/react-native-web/src/exports/InteractionManager/index.js
+++ packages/react-native-web/src/exports/InteractionManager/index.js
@@ -9,9 +9,16 @@
  */
 
 import invariant from 'fbjs/lib/invariant';
-import requestIdleCallback, {
-  cancelIdleCallback
-} from '../../modules/requestIdleCallback';
+import type { Task } from './TaskQueue';
+import TaskQueue from './TaskQueue';
+import type { EventSubscription } from '../../vendor/react-native/emitter/EventEmitter';
+import EventEmitter from '../../vendor/react-native/emitter/EventEmitter';
+import { setImmediate } from './immediateShim';
+
+const _emitter = new EventEmitter<{
+  interactionComplete: [],
+  interactionStart: []
+}>();
 
 const InteractionManager = {
   Events: {
@@ -22,27 +29,28 @@ const InteractionManager = {
   /**
    * Schedule a function to run after all interactions have completed.
    */
-  runAfterInteractions(task: ?Function): {
+  runAfterInteractions(task: ?Task): {
     then: Function,
     done: Function,
     cancel: Function
   } {
-    let handle;
-
+    const tasks: Array<Task> = [];
     const promise = new Promise((resolve) => {
-      handle = requestIdleCallback(() => {
-        if (task) {
-          resolve(task());
-        } else {
-          resolve();
-        }
+      _scheduleUpdate();
+      if (task) {
+        tasks.push(task);
+      }
+      tasks.push({
+        run: resolve,
+        name: 'resolve ' + ((task && task.name) || '?')
       });
+      _taskQueue.enqueueTasks(tasks);
     });
     return {
       then: promise.then.bind(promise),
       done: promise.then.bind(promise),
       cancel: () => {
-        cancelIdleCallback(handle);
+        _taskQueue.cancelTasks(tasks);
       }
     };
   },
@@ -51,7 +59,10 @@ const InteractionManager = {
    * Notify manager that an interaction has started.
    */
   createInteractionHandle(): number {
-    return 1;
+    _scheduleUpdate();
+    const handle = ++_inc;
+    _addInteractionSet.add(handle);
+    return handle;
   },
 
   /**
@@ -59,9 +70,73 @@ const InteractionManager = {
    */
   clearInteractionHandle(handle: number) {
     invariant(!!handle, 'Must provide a handle to clear.');
+    _scheduleUpdate();
+    _addInteractionSet.delete(handle);
+    _deleteInteractionSet.add(handle);
   },
 
-  addListener: () => {}
+  addListener: (_emitter.addListener.bind(_emitter): EventSubscription),
+
+  /**
+   *
+   * @param deadline
+   */
+  setDeadline(deadline: number) {
+    _deadline = deadline;
+  }
 };
+
+const _interactionSet = new Set();
+const _addInteractionSet = new Set();
+const _deleteInteractionSet = new Set();
+const _taskQueue = new TaskQueue({ onMoreTasks: _scheduleUpdate });
+let _nextUpdateHandle: TimeoutID | number = 0;
+let _inc = 0;
+let _deadline = -1;
+
+/**
+ * Schedule an asynchronous update to the interaction state.
+ */
+function _scheduleUpdate() {
+  if (!_nextUpdateHandle) {
+    if (_deadline > 0) {
+      _nextUpdateHandle = setTimeout(_processUpdate);
+    } else {
+      _nextUpdateHandle = setImmediate(_processUpdate);
+    }
+  }
+}
+
+/**
+ * Notify listeners, process queue, etc
+ */
+function _processUpdate() {
+  _nextUpdateHandle = 0;
+  const interactionCount = _interactionSet.size;
+  _addInteractionSet.forEach((handle) => _interactionSet.add(handle));
+  _deleteInteractionSet.forEach((handle) => _interactionSet.delete(handle));
+  const nextInteractionCount = _interactionSet.size;
+
+  if (interactionCount !== 0 && nextInteractionCount === 0) {
+    _emitter.emit(InteractionManager.Events.interactionComplete);
+  } else if (interactionCount === 0 && nextInteractionCount !== 0) {
+    _emitter.emit(InteractionManager.Events.interactionStart);
+  }
+
+  if (nextInteractionCount === 0) {
+    // It seems that we can't know the running time of the current event loop,
+    // we can only calculate the running time of the current task queue.
+    const begin = Date.now();
+    while (_taskQueue.hasTasksToProcess()) {
+      _taskQueue.processNext();
+      if (_deadline > 0 && Date.now() - begin >= _deadline) {
+        _scheduleUpdate();
+        break;
+      }
+    }
+  }
+  _addInteractionSet.clear();
+  _deleteInteractionSet.clear();
+}
 
 export default InteractionManager;
