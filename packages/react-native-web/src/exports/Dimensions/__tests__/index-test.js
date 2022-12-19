ce5be31de4ae4819d2e98aced0680262d8ed8c28
Yogendra Rawal <yogen.rawal@gmail.com>
2022-12-19 23:26:36 +0545

    Feat: Add tests for new VisualViewport changes

--- packages/react-native-web/src/exports/Dimensions/__tests__/index-test.js
+++ packages/react-native-web/src/exports/Dimensions/__tests__/index-test.js
@@ -55,3 +55,60 @@ describe('apis/Dimensions', () => {
     expect(handler).toHaveBeenCalledTimes(0);
   });
 });
+
+describe('apis/Dimensions(VisualViewport)', () => {
+  window.visualViewport = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scale: 1,
+    dispatchEvent: (event) => window.dispatchEvent(event)
+  };
+
+  test('get(VisualViewport)', () => {
+    const handler = jest.fn();
+    Dimensions.addEventListener('change', handler);
+    expect(Dimensions.get('screen')).toMatchInlineSnapshot(`
+      {
+        "fontScale": 1,
+        "height": 0,
+        "scale": 1,
+        "width": 0,
+      }
+    `);
+    expect(Dimensions.get('window')).toMatchInlineSnapshot(`
+      {
+        "fontScale": 1,
+        "height": 768,
+        "scale": 1,
+        "width": 1024,
+      }
+    `);
+    expect(handler).toHaveBeenCalledTimes(0);
+  });
+
+  test('set(VisualViewport)', () => {
+    expect(() => Dimensions.set({})).toThrow();
+  });
+
+  test('addEventListener(VisualViewport)', () => {
+    const handler = jest.fn();
+    const subscription = Dimensions.addEventListener('change', handler);
+    console.log('window.visualViewport', window.visualViewport);
+    window.visualViewport.dispatchEvent(new Event('resize'));
+    expect(handler).toHaveBeenCalledTimes(1);
+    expect(handler).toHaveBeenLastCalledWith({
+      window: Dimensions.get('window'),
+      screen: Dimensions.get('screen')
+    });
+    subscription.remove();
+    window.visualViewport.dispatchEvent(new Event('resize'));
+    expect(handler).toHaveBeenCalledTimes(1);
+  });
+
+  test('removeEventListener(VisualViewport)', () => {
+    const handler = jest.fn();
+    Dimensions.removeEventListener('change', handler);
+    window.visualViewport.dispatchEvent(new Event('resize'));
+    expect(handler).toHaveBeenCalledTimes(0);
+  });
+});
