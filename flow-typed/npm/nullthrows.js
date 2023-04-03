5eedbdaca0147a1d47a94d011ae604f3d2a6d5ef
Lucio Bertin Chávez Fuentes <luciobertinchavez@gmail.com>
2023-04-03 16:26:06 -0600

    Revert "declare modules flow types"
    This reverts commit bbf9c1d29fb13155e7b47b9b7ed8216683f60169.

--- flow-typed/npm/nullthrows.js
+++
@@ -1,6 +0,0 @@
-//Copied from: https://github.com/zertosh/nullthrows/blob/master/nullthrows.js.flow
-declare module 'nullthrows' {
-  /* @flow strict */
-
-  declare module.exports: <T>(x: ?T, message?: string) => T;
-}
