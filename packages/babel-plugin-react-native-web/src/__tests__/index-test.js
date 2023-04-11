cca0fb16451563871c631a03c299805393e3224a
Nicolas Gallagher <necolas@meta.com>
2023-04-11 16:15:47 -0700

    [change] babel-plugin option for style runtime

--- packages/babel-plugin-react-native-web/src/__tests__/index-test.js
+++ packages/babel-plugin-react-native-web/src/__tests__/index-test.js
@@ -1,69 +1,86 @@
 const plugin = require('..');
 const pluginTester = require('babel-plugin-tester').default;
 
-const tests = [
-  // import react-native
-  {
-    title: 'import from "react-native"',
-    code: `import ReactNative from 'react-native';
-import { View } from 'react-native';
+function createTests(pluginOptions) {
+  return [
+    // import react-native
+    {
+      title: 'import from "react-native"',
+      code: `import ReactNative from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Invalid, View as MyView } from 'react-native';
 import { useLocaleContext } from 'react-native';
 import * as ReactNativeModules from 'react-native';`,
-    snapshot: true
-  },
-  {
-    title: 'import from "react-native"',
-    code: `import ReactNative from 'react-native';
-import { View } from 'react-native';
-import { Invalid, View as MyView } from 'react-native';
-import * as ReactNativeModules from 'react-native';`,
-    snapshot: true,
-    pluginOptions: { commonjs: true }
-  },
-  {
-    title: 'import from "react-native-web"',
-    code: `import { unstable_createElement } from 'react-native-web';
+      snapshot: true,
+      pluginOptions
+    },
+    {
+      title: 'import from "react-native-web"',
+      code: `import { unstable_createElement } from 'react-native-web';
 import { StyleSheet, View, Pressable, processColor } from 'react-native-web';
 import * as ReactNativeModules from 'react-native-web';`,
-    snapshot: true
-  },
-  {
-    title: 'export from "react-native"',
-    code: `export { View } from 'react-native';
+      snapshot: true,
+      pluginOptions
+    },
+    {
+      title: 'export from "react-native"',
+      code: `export { View } from 'react-native';
 export { StyleSheet, Text, unstable_createElement } from 'react-native';`,
-    snapshot: true
-  },
-  {
-    title: 'export from "react-native-web"',
-    code: `export { View } from 'react-native-web';
+      snapshot: true,
+      pluginOptions
+    },
+    {
+      title: 'export from "react-native-web"',
+      code: `export { View } from 'react-native-web';
 export { StyleSheet, Text, unstable_createElement } from 'react-native-web';`,
-    snapshot: true
-  },
-  // require react-native
-  {
-    title: 'require "react-native"',
-    code: `const ReactNative = require('react-native');
+      snapshot: true,
+      pluginOptions
+    },
+    // require react-native
+    {
+      title: 'require "react-native"',
+      code: `const ReactNative = require('react-native');
 const { View } = require('react-native');
 const { StyleSheet, Pressable } = require('react-native');`,
-    snapshot: true
-  },
-  {
-    title: 'require "react-native"',
-    code: `const ReactNative = require('react-native');
-const { View } = require('react-native');
-const { StyleSheet, Pressable } = require('react-native');`,
-    snapshot: true,
-    pluginOptions: { commonjs: true }
-  },
-  {
-    title: 'require "react-native-web"',
-    code: `const ReactNative = require('react-native-web');
+      snapshot: true,
+      pluginOptions
+    },
+    {
+      title: 'require "react-native-web"',
+      code: `const ReactNative = require('react-native-web');
 const { unstable_createElement } = require('react-native-web');
 const { StyleSheet, View, Pressable, processColor } = require('react-native-web');`,
-    snapshot: true
-  }
-];
+      snapshot: true,
+      pluginOptions
+    }
+  ];
+}
+
+pluginTester({
+  babelOptions: {
+    generatorOpts: {
+      jsescOption: {
+        quotes: 'single'
+      }
+    }
+  },
+  plugin,
+  pluginName: '[legacy] Rewrite react-native to react-native-web',
+  tests: createTests({})
+});
+
+pluginTester({
+  babelOptions: {
+    generatorOpts: {
+      jsescOption: {
+        quotes: 'single'
+      }
+    }
+  },
+  plugin,
+  pluginName: '[commonjs] Rewrite react-native to react-native-web',
+  tests: createTests({ commonjs: true })
+});
 
 pluginTester({
   babelOptions: {
@@ -75,5 +92,5 @@ pluginTester({
   },
   plugin,
   pluginName: 'Rewrite react-native to react-native-web',
-  tests
+  tests: createTests({ legacy: false })
 });
