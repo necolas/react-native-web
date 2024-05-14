b8aa3973c9b9726df8992f90769dd05163ac7c15
Tienifr <christianwen18@gmail.com>
2024-05-14 19:22:18 +0700

    revert: passive wheel event listener in virtualized list

--- packages/react-native-web/src/vendor/react-native/VirtualizedList/index.js
+++ packages/react-native-web/src/vendor/react-native/VirtualizedList/index.js
@@ -747,7 +747,6 @@ class VirtualizedList extends StateSafePureComponent<Props, State> {
     if (this._scrollRef && this._scrollRef.getScrollableNode) {
       this._scrollRef.getScrollableNode().addEventListener('wheel',
           this.invertedWheelEventHandler,
-          { passive: true },
       );
     } else {
       setTimeout(() => this.setupWebWheelHandler(), 50);
