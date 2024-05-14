1e610adfcb67df6764af713a221a4375c8233de6
Tienifr <christianwen18@gmail.com>
2024-05-14 19:25:21 +0700

    remove redundant comma

--- packages/react-native-web/src/vendor/react-native/VirtualizedList/index.js
+++ packages/react-native-web/src/vendor/react-native/VirtualizedList/index.js
@@ -746,7 +746,7 @@ class VirtualizedList extends StateSafePureComponent<Props, State> {
   setupWebWheelHandler() {
     if (this._scrollRef && this._scrollRef.getScrollableNode) {
       this._scrollRef.getScrollableNode().addEventListener('wheel',
-          this.invertedWheelEventHandler,
+          this.invertedWheelEventHandler
       );
     } else {
       setTimeout(() => this.setupWebWheelHandler(), 50);
