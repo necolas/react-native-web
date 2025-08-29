bb7c6d1b6a175c46e9b457e27b191ed8b9bd6d39
staszekscp <mariusz.stanisz@swmansion.com>
2025-08-29 08:05:05 +0200

    Remove code to fix pointerEvents inline styling

--- packages/react-native-web/src/modules/createDOMProps/index.js
+++ packages/react-native-web/src/modules/createDOMProps/index.js
@@ -879,11 +879,9 @@ const createDOMProps = (elementType, props, options) => {
       `props.pointerEvents is deprecated. Use style.pointerEvents`
     );
   }
-  const pointerEventsValue =
-    StyleSheet.flatten(style).pointerEvents || pointerEvents;
 
   const [className, inlineStyle] = StyleSheet(
-    [style, pointerEventsValue && pointerEventsStyles[pointerEventsValue]],
+    [style, pointerEvents && pointerEventsStyles[pointerEvents]],
     {
       writingDirection: 'ltr',
       ...options
